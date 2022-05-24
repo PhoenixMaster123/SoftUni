40c33252e3a378af0018e42eebadd7becb5c0daf
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:25:32 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+            int day = int.Parse(Console.ReadLine());
+            if (day >= 1 && day <= days.Length) 
+            {
+                Console.WriteLine(days[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+         
+        }
+    }
+}
