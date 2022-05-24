a7653ba5a7db21ddda61a0b5c83f768adea7a47a
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:31:01 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Exercise/04. Array Rotation - Exercese/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Array_Rotation___Exercese
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] items = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rotetionsCount = int.Parse(Console.ReadLine());
+           
+            for (int rot = 1; rot <= rotetionsCount; rot++)
+            {
+                int firstElement = items[0];
+                for (int i = 0; i <= items.Length - 2; i++)
+                {
+                    items[i] = items[i + 1];
+                }
+                items[items.Length - 1] = firstElement;
+            }
+           
+            Console.WriteLine(string.Join(" ",items));
+        }
+    }
+}
