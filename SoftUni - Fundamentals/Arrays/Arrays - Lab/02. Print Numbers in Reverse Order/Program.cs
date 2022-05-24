6f8bf056636b95de900a79a1d54ccc8c677a0d4b
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:26:28 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Lab/02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int n = int.Parse(Console.ReadLine());
+            int[] number = new int[n];
+            for (int i = 0;i<n;i++)
+            {
+                number[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = number.Length-1; i >= 0; i--)
+
+            {
+                Console.Write(number[i] + " "); 
+            }
+           // Console.WriteLine(string.Join(" ", number)); // short nachin na cikula
+            Console.WriteLine();
+        }
+    }
+}
