28a66720814e6db9bcb069f592d4c52aa333862d
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:20:55 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+           string [] words = Console.ReadLine().Split(' ');
+            string[] values = new string[words.Length];
+            for (int i = 0;i<values.Length;i++)
+            {
+                values[i] = words[i].Trim();
+            }
+            for (int i = values.Length - 1; i >= 0; i--)
+
+            {
+                Console.Write(values[i] + " ");
+            }
+          //  Console.WriteLine(string.Join(" ", values));
+        }
+    }
+}
