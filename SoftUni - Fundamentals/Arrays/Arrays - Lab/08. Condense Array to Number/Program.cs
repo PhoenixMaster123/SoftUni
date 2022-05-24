ccf21d9ff1c967228d67770f42866f8dd42fb5b9
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:24:18 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] items = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int firstLenght = items.Length;
+            for (int i = 0; i < firstLenght-1; i++)
+            {
+                int[] condensed = new int[items.Length-1];
+                for (int j = 0; j < items.Length-1; j++)
+                {
+                    condensed[j] = items[j] + items[j + 1];
+                }
+                items = condensed;
+            }
+            Console.WriteLine(items[0]);
+        }
+    }
+}
