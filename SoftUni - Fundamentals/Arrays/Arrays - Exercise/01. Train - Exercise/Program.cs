c90e37e593f09a9a61870f825cff1bf8c06707d7
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:29:12 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Exercise/01. Train - Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Train___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] number = new int[count];
+            int sum = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = int.Parse(Console.ReadLine());
+                sum+=number[i];
+                
+            }
+            Console.WriteLine(string.Join(" ", number));
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
