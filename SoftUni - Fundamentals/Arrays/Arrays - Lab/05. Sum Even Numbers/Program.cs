88df4d8071f7404fcf5969aaee229a513d26efc4
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:22:02 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    int currentValue = numbers[i];
+               
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        sum += currentValue;                  
+            //    }
+               
+            //}
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
