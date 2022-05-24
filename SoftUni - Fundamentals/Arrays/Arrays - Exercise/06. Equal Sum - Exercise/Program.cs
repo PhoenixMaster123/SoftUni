be9adaf284e1c7994ca341ff44fb8d1753b12534
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:32:29 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Exercise/06. Equal Sum - Exercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int[] number = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            bool isFound = false;
+            for (int i = 0; i < number.Length; i++)
+            {
+                int leftSum = 0;
+                for(int j=0; j<i; j++)
+                {
+                    leftSum+=number[j];
+                }
+                int rightSum = 0;
+                for (int k = number.Length-1; k > i; k--)
+                {
+                    rightSum += number[k];
+                }
+                if (leftSum == rightSum && !isFound)
+                {
+                    Console.WriteLine(i);
+                    isFound = true;
+                }
+            }
+            if(!isFound)
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
