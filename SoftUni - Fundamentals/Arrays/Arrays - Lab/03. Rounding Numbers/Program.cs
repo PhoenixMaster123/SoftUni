ae0a0689b13e437c8e01c2d744740fabaaef569e
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:27:12 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] number = Console.ReadLine().Split().Select(double.Parse).ToArray();           
+            for (int i = 0; i < number.Length; i++)
+            {                
+                Console.WriteLine($"{number[i]} => {Math.Round(number[i],MidpointRounding.AwayFromZero)}");
+            }
+            /////////////////////////////////////// Variant 2 ///////////////////////////////////////////////////////////
+            //string[] rawinput = Console.ReadLine().Split();
+            //double[] items = new double[number.Length];
+            //for (int i = 0; i < rawinput.Length; i++)
+            //{
+            //    items[i] = double.Parse(rawinput[i]);
+            //}
+            //for (int i = 0; i < items.Length; i++)
+            //{
+            //    Console.WriteLine($"{items[i]} => {Math.Round(items[i], MidpointRounding.AwayFromZero)}");
+            //}
+
+        }
+    }
+}
