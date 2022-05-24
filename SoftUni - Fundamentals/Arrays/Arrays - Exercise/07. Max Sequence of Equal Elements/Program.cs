703545c1d07a36f4e3651c8dcf295e09ada92c51
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-24 22:33:27 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int count = 1;
+            int dublicate = 0;
+            int index = arr[0];
+            int bestCandidate = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (index == arr[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    index = arr[i];
+                    count = 1;
+                }
+                if (count > bestCandidate)
+                {
+                    bestCandidate = count;
+                    dublicate = arr[i];
+                }
+            }
+            for (int i = 0; i < bestCandidate; i++)
+            {
+                Console.Write($"{dublicate} ");
+            }
+        }
+    }
+}
