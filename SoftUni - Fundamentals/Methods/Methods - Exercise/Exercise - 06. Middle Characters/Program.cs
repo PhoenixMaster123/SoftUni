c6676436edb1aeb9a8e1374dcb1cf42018b171b1
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:28:52 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Exercise/Exercise - 06. Middle Characters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise___06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();         
+            Console.WriteLine(PrintMidString(word));
+        }
+        static string PrintMidString(string input)
+        {
+            int len = input.Length;
+            string result = "";
+
+            if (len % 2 == 1)
+            {
+                result = input[input.Length / 2].ToString();
+            }
+            else
+            {
+                result = input[input.Length / 2 - 1].ToString() + input[input.Length / 2].ToString();
+            }
+
+            return result;
+        }
+    }
+    }
+
