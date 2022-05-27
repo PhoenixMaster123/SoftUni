f1ef1ca8187085f77ae58ff41e8b5bf3c428f891
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:19:07 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Lab/11. Math operations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(MathFunction(num1,symbol,num2));
+        }
+        static int MathFunction(int num1, char symbol, int num2)
+        {
+            int result = 0;
+            if (symbol == '*')
+            {
+                result = num1 * num2;
+            }
+            else if(symbol == '+')
+            {
+                result = num1 + num2;
+            }
+            else if(symbol == '-')
+            {
+                result = num1 - num2;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
+            return result;
+        }
+    }
+}
