8ed05857bef3abd6111b4f00df67fb8343fd41c7
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:18:30 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            var sumOfEvenDigits = GetSumOfEvenDigits(number);
+            var sumOfOddDigits = GetSumOfOddDigits(number);
+            Console.WriteLine(sumOfEvenDigits*sumOfOddDigits);
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == 0)
+                {
+                    sum += number % 10;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 != 0)
+                {
+                    sum += number % 10;
+                }
+                number /= 10;
+            }
+            return sum;
+        }
+   
+    }
+}
