6a4392c40a46712a5b452f46daf23c11ac027bd5
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:15:01 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Lab/05. Orders/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+           
+            Caffee(command, count);
+            
+            
+        }
+        static void Caffee(string product, int count)
+        {
+            double price = 0;
+            if (product == "coffee")
+            {
+                price = count * 1.50;
+               
+            }
+            else if(product=="water")
+            {
+                price = count * 1.00;
+               
+
+            }
+            else if (product == "coke")
+            {
+                price = count * 1.40;
+
+            }
+            else
+            {
+                price = count * 2.00;
+               
+            }
+            Console.WriteLine($"{price:F2}");
+
+            
+
+        }
+           
+
+    }
+}
