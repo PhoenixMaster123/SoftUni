611f752e2b5c98fc3136385d92a92fa8d5e3b247
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:31:15 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Exercise/Exercise - 08. Factorial Division/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise___08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int factorial = int.Parse(Console.ReadLine());
+            int devide = int.Parse(Console.ReadLine());
+
+            double firstFac = Factorial(factorial);
+            double secondFac = Factorial(devide);
+
+            double result = firstFac / secondFac;
+           
+            Console.WriteLine($"{result:F2}");
+        }
+        static double Factorial(double factorial)
+        {
+            double result = factorial;
+                           
+                for (int k = 1; k < factorial; k++)
+                {
+                    result *= k;
+                }
+                       
+            return result;
+        }
+    }
+}
