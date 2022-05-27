4cd00fea2459e2918d75510e816db7482cc2b106
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:29:49 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Exercise/Exercise - 07. NxN Matrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise___07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////////// First Variant //////////////////////////////
+           
+            // int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int k = 1; k <= n; k++)
+            //    {
+            //        Console.Write($"{n} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            ///////////////////////////////////////////////// Second Variant ////////////////////////////////
+            int n = int.Parse(Console.ReadLine());          
+            Console.WriteLine(NxNMatrix(n));
+        }
+        static int NxNMatrix(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = 1; k <= n; k++)
+                {
+                    Console.Write($"{n} ");
+                }
+                Console.WriteLine();
+               
+            }
+            return n;
+            
+           
+           
+        }
+    }
+}
