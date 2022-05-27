b5b47319ba6114a9d1d556c2a006a62aaa0ec577
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:28:00 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Exercise/Exercise - 05. Add and Subtract/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exercise___05._Add_and_Subtract
+{
+    internal class Program
+    {
+        /////////////////////////////////////////////// First Variant - My ///////////////////////////////
+        //static void Main(string[] args)
+        //{
+        //    int num1 = int.Parse(Console.ReadLine());
+        //    int num2 = int.Parse(Console.ReadLine());
+        //    int num3 = int.Parse(Console.ReadLine());
+
+        //    Console.WriteLine(SumFirstAndSecond(num1,num2,num3));
+        //}
+        //static int SumFirstAndSecond(int num1, int num2, int num3)
+        //{
+        //    int result = 0;
+        //    result = (num1 + num2)-num3;
+        //    return result;
+        //}
+        ////////////////////////////////////////////////////// Second - My /////////////////////////////////////////////
+
+
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int result = SumFirstAndSecond(num1, num2);        
+            Console.WriteLine(Substract(result, num3));
+        }
+        static int SumFirstAndSecond(int num1, int num2)
+        {
+            int result = 0;
+            result = num1 + num2;
+            return result;
+
+        }
+        static int Substract(int result, int num3)
+        {
+            result -= num3;
+            return result;
+        }
+    }
+}
