e33099976700c4ed9742c2c25a8425d18705e9a3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:36:35 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - More Exercise/More Exercise - 01. Data Types/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace More_Exercise___01._Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            
+            if(command == "int")
+            {
+                int number = int.Parse(Console.ReadLine());
+                int result = Multiply(number);
+                Console.WriteLine(result);
+            }
+            else if(command == "real")
+            {
+                double number = double.Parse(Console.ReadLine());
+                double result = Multiply(number);
+                Console.WriteLine($"{result:F2}");
+            }
+            else
+            {
+                string message = Console.ReadLine();
+                string result = Multiply(message);
+                Console.WriteLine($"${result}$");
+            }
+        }
+        static int Multiply(int number)
+        {
+            int result = 0;
+            result = number*2;
+            return result;
+        }
+        static double Multiply(double number)
+        {
+            
+            return number * 1.5;
+        }
+
+        static string Multiply(string command)
+        {           
+            return command;
+        }
+    }
+}
