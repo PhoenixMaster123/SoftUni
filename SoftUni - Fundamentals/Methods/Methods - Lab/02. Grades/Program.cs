73596c0154ba10d900cf23ad1403337d01136948
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:10:54 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Lab/02. Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double grade = double.Parse(Console.ReadLine());
+            PrintGrade(grade);
+        }
+        static void PrintGrade(double note)
+        {
+            if (note >= 2.00 && note <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (note >= 3.00 && note <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (note >= 3.50 && note <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (note >= 4.50 && note <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
