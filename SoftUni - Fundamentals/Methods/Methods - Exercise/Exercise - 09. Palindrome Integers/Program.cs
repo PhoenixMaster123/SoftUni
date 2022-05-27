5dc55f6f45c5f448182b3592522c832c6383d5b0
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:31:56 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Exercise/Exercise - 09. Palindrome Integers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise___09._Palindrome_Integers
+{
+    internal class Program
+    {       
+            public static void Main(string[] args)
+            {
+                string command = Console.ReadLine();
+
+                Palindrome(command);
+            }
+
+            private static void Palindrome(string command)
+            {
+                while (command != "END")
+                {
+                    bool isPalindrome = true;
+                    for (int i = 0; i < command.Length; i++)
+                    {
+                        if (!(command[i] == command[command.Length - i - 1]))
+                        {
+                            isPalindrome = false;
+                            Console.WriteLine("false");
+                            break;
+                        }
+                    }
+
+                    if (isPalindrome)
+                    {
+                        Console.WriteLine("true");
+
+                    }
+
+                    command = Console.ReadLine();
+                }
+            }
+        }
+        }
+  
