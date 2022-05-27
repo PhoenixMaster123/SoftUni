8df603e80aeb3d7980350521399ace2f1006712f
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:08:56 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = int.Parse(Console.ReadLine());
+            Number(number);
+        }
+        static void Number(int num)
+        {
+            if(num>0)
+            {
+                Console.WriteLine($"The number {num} is positive.");
+            }
+            else if(num<0)
+            {
+                Console.WriteLine($"The number {num} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {num} is zero.");
+            }
+        }
+
+    }
+}
