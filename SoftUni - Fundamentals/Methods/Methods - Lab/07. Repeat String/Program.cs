888de963b497ce5530a534825b5c5f44a5a889e1
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-27 18:16:50 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Methods/Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        //////////////////////////////////// My Variant /////////////////////////////////////
+        //static void Main(string[] args)
+        //{
+        //  string word = Console.ReadLine();
+        //  int count = int.Parse(Console.ReadLine());
+        //    CopyWord(word, count);
+        //}
+        //static void CopyWord(string word, int count)
+        //{
+        //    for (int i = 1; i <= count; i++)
+        //    {
+        //        Console.Write(word);
+        //    }
+        //}
+        ///////////////////////////////// Second Variant ////////////////////////////
+        //static void Main(string[] args)
+        //{
+        //    string templete = Console.ReadLine();
+        //    int repates = int.Parse(Console.ReadLine());
+        //    Console.WriteLine(RepeatString(templete, repates)); 
+        //}
+        //static string RepeatString(string template, int repates)
+        //{
+        //    string result = string.Empty; // create always new strings
+        //    for (int i = 0; i < repates; i++)
+        //    {
+        //        result+=template; // slow
+        //    }
+        //    return result;
+        //}
+        ///////////////////////////////// Third Variant //////////////////////////// Best Variant
+        static void Main(string[] args)
+        {
+            string templete = Console.ReadLine();
+            int repates = int.Parse(Console.ReadLine());
+            Console.WriteLine(RepeatString(templete, repates));
+        }
+        static string RepeatString(string template, int repates)
+        {
+            StringBuilder stringBuilder = new StringBuilder(); // never create new string, but make massiv string!!! faster
+            for (int i = 0; i < repates; i++)
+            {
+                stringBuilder.Append(template);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
