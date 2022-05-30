a03c232ccce867f6140dd8d5c2088b442bb8a9c9
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-31 00:40:11 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Objects and Classes/Objects and Classes - Exercise/Exercise - 06. Vehicle Catalogue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise___06._Vehicle_Catalogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string command = Console.ReadLine();
+            while(command !="End")
+            {
+
+
+
+                command = Console.ReadLine();
+            }
+        }
+        class Vehicle
+        {
+            public Vehicle(string type, string model, string color, int horsePower)
+            {
+                this.Type = type;
+                this.Model = model;
+                this.Color = color;
+                this.HorsePower = horsePower;
+            }
+            public string Type { get; set; }
+            public string Model { get; set; }
+            public string Color { get; set; }
+            public int HorsePower { get; set; }
+
+        }
+        
+    }
+}
