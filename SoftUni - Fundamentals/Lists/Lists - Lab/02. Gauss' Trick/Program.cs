2b913a20487440f2e7163921ad90cf1a94aff763
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-30 22:10:33 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Lists/Lists - Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+            for (int i = 0; i < num.Count / 2; i++)
+            { 
+                int newElement = num[i] + num[num.Count - 1 - i];
+                result.Add(newElement);
+                
+            }
+            if(num.Count % 2 != 0)
+            {
+                result.Add(num[num.Count / 2]);
+            }
+            Console.WriteLine(string.Join(' ',result));
+           
+
+        }
+    }
+}
