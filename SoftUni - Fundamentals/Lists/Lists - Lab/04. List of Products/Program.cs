a7d7fd3555d6a7567da7a4b9d8d5d7f8fc1128e1
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-30 22:12:33 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Lists/Lists - Lab/04. List of Products/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+           
+            for (int i = 1; i <= number ; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+      
+            }
+            products.Sort();
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.{products[i]}");
+            }
+
+        }
+
+    }
+}
