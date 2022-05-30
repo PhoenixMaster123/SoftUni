9c2566ee3de95c1d7943035120f0b76cec67d37c
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-30 22:33:49 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Lists/Lists - Exercise/Exercise - 07. Append Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace listExer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<string> listOfLists = Console.ReadLine().Split('|').ToList();
+            List<string> result = new List<string>();
+
+            for (int i = listOfLists.Count - 1; i >= 0; i--)
+            {
+                var currentList = listOfLists[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in currentList)
+                {
+                    result.Add(item);
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
