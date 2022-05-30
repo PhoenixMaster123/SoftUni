00834df7dffc01534f31c4a99270990d7bcae331
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-31 00:31:40 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Objects and Classes/Objects and Classes - Lab/02. Big Factorial - Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02._Big_Factorial___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+
+                result *= i;
+               
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
