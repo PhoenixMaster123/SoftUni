432d52804a03c0edc505cbb3135873607043716b
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-31 00:30:28 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Objects and Classes/Objects and Classes - Lab/01. Randomize Words - Lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Randomize_Words___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+           Random random = new Random();
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(0, words.Length);
+                string currentWord = words[i];
+                words[i] = words[randomIndex];
+                words[randomIndex] = currentWord;
+               
+            }
+            foreach (string word in words)
+            {
+                
+                Console.WriteLine(word);
+            }
+           // Console.WriteLine(random.Next(1,10)); // generate random number
+        }
+    }
+}
