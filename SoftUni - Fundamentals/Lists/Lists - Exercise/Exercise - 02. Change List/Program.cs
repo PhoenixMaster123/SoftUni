bcf5fe92976cb3ad7b01a34d2f048fb325ce8b2b
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-30 22:21:07 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Lists/Lists - Exercise/Exercise - 02. Change List/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise___02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.List<int> numbers = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+
+            string command = Console.ReadLine();
+            while(command != "end")
+            {
+                
+                string[] commands = command.Split().ToArray();
+                string checkForAdd = commands[0];
+                if (checkForAdd == "Delete")
+                {
+                    int deleteNumber = int.Parse(commands[1]);
+                    numbers.Remove(deleteNumber);
+                    if(numbers.Contains(deleteNumber))
+                    {
+                        numbers.Remove(deleteNumber);
+                    }    
+                }
+                else if(checkForAdd == "Insert")
+                {
+                    int number = int.Parse(commands[1]);
+                    int second = int.Parse(commands[2]);
+                    numbers.Insert(second, number);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
