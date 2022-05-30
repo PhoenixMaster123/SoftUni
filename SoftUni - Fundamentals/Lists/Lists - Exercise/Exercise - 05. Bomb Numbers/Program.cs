2a2da72714e103fcce1f927ad08d51474a1fb59d
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-30 22:32:13 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Lists/Lists - Exercise/Exercise - 05. Bomb Numbers/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise___05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int[] bombInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int bombNumber = bombInfo[0];
+            int bombPower = bombInfo[1];
+
+            //int indexOfBomb;
+            //while((indexOfBomb = numbers.IndexOf(bombNumber)) >= 0)
+            //{
+
+            //}
+
+            while (true)
+            {
+                int indexOfBomb = numbers.IndexOf(bombNumber);
+                if (indexOfBomb == -1)
+                {
+                    // There no more bombs inside the loop
+                    break;
+                }
+                else
+                {
+                    DetonateBomb(numbers, indexOfBomb, bombPower);
+                }
+            }
+            Console.WriteLine(numbers.Sum());
+
+
+        }
+
+
+        // For a single bomb
+        static void DetonateBomb(List<int> numbers, int indexOfBomb, int bombPower)
+        {
+
+            int rightCount = indexOfBomb + bombPower;
+            for (int i = indexOfBomb; i <= rightCount; i++)
+            {
+                if (indexOfBomb >= numbers.Count)
+                {
+                    break;
+                }
+                else
+                {
+                    numbers.RemoveAt(indexOfBomb);
+                }
+            }
+            int leftCount = indexOfBomb - bombPower;
+            if (leftCount < 0)
+            {
+                leftCount = 0;
+            }
+            for (int count = leftCount; count < indexOfBomb; count++)
+            {
+                if (leftCount >= numbers.Count)
+                {
+                    break;
+                }
+                else
+                {
+                    numbers.RemoveAt(leftCount);
+                }
+
+            }
+        }
+    }
+}
+
