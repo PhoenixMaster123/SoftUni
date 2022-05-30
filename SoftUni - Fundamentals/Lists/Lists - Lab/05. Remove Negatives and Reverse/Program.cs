1f8d4213c3bf5aa2f9e9913b34cae0410efa5492
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-30 22:13:51 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Lists/Lists - Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////////////////////////////////////////////////////// First Variant ///////////////////////////////////////
+            //List<int> numbers =Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            //List <int> result = new List<int>();
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    if(numbers[i] < 0)
+            //    {
+            //        numbers.RemoveAt(i);
+            //        i--;
+            //    }
+            //    else
+            //    {
+            //        result.Add(numbers[i]);
+            //    }
+            //}
+            //result.Reverse();
+            //if(result.Count == 0)
+            //{
+            //    Console.WriteLine("empty");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(string.Join(" ",result));
+            //}
+            ///////////////////////////////////////////////////////// Second Variant //////////////////////////////////////
+            List<int> numbers =Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            numbers.RemoveAll(x => x < 0);
+            numbers.Reverse();
+            if(numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+            
+        }
+    }
+}
