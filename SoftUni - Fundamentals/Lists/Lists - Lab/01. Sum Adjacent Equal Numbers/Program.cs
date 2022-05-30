255b997c054096873053b05a98a53f3551830220
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-05-30 22:08:56 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Lists/Lists - Lab/01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> number = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+
+            for (int i = 1; i < number.Count; i++)
+            {
+                if (number[i] == number[i - 1])
+                {
+                    number[i] = number[i] + number[i - 1];
+                    number.Remove(number[i - 1]);
+                    i = 0;
+                }
+            }
+            Console.WriteLine(string.Join(" ",number));
+        }
+    }
+}
