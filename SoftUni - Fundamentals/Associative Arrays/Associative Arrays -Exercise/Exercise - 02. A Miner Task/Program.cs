a8c8b090d0bd2f026e63583b24eae24be1c4d962
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-01 01:49:09 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Associative Arrays/Associative Arrays -Exercise/Exercise - 02. A Miner Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise___02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> treasure = new Dictionary<string, int>();
+            while(true)
+            {
+                string items = Console.ReadLine();              
+                if(items == "stop")
+                {
+                    break;
+                }
+                int value = int.Parse(Console.ReadLine());
+               
+                if (treasure.ContainsKey(items))
+                {
+                    treasure[items]+=value;
+                }
+                else
+                {
+                    treasure.Add(items, value);
+                }
+              
+                
+            }
+            foreach(var item in treasure)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
