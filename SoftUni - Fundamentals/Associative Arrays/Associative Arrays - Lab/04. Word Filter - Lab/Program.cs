a329304f44db0c83b25270156a33b81c40d33387
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-01 01:45:42 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Associative Arrays/Associative Arrays - Lab/04. Word Filter - Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Word_Filter___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine()
+                                       .Split(" ").Where(x => x.Length % 2 == 0)
+                                      .ToArray();
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Console.WriteLine(elements[i]);
+            }
+        }
+    }
+}
