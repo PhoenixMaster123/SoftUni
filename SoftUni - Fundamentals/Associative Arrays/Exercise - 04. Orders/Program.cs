32f1ef174f8cde1ef91c75b9c92aadddbcab8dda
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-01 01:55:43 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Associative Arrays/Exercise - 04. Orders/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> totalPrice = new Dictionary<string, double>();
+            Dictionary<string, int> totalQuantity = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+
+                string product = input[0];
+
+                if (totalPrice.ContainsKey(product) && product != "buy")
+                {
+                    double price = double.Parse(input[1]);
+                    int quantity = int.Parse(input[2]);
+
+                    totalPrice[product] = price;
+                    totalQuantity[product] += quantity;
+                }
+
+                else if (product != "buy")
+                {
+                    double price = double.Parse(input[1]);
+                    int quantity = int.Parse(input[2]);
+
+                    totalQuantity.Add(product, quantity);
+                    totalPrice.Add(product, price);
+                }
+
+                else if (product == "buy")
+                {
+                    foreach (var p in totalQuantity)
+                    {
+                        string Product = p.Key;
+                        int quantity = p.Value;
+                        double price = totalPrice[Product];
+                        double results = quantity * price;
+                        Console.WriteLine($"{Product} -> {results:f2}");
+                    }
+                    break;
+                }
+
+            }
+        }
+    }
+}
