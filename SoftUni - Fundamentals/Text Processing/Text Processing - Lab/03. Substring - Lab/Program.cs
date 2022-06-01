90cf8b29eb38a0ecfadf5c16b243ec3e8e77fb7e
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-02 00:04:41 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Text Processing/Text Processing - Lab/03. Substring - Lab/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Substring___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////////////////////////////////////// Variant 1 ////////////////////////////////////////
+            //string wordRemove = Console.ReadLine();
+            //string text = Console.ReadLine();
+
+            // while(text.Contains(wordRemove))
+            // {
+            //     int startIndexWordOfRemove = text.IndexOf(wordRemove);
+            //     text = text.Remove(startIndexWordOfRemove, wordRemove.Length);
+
+            // }
+            // Console.WriteLine(text);
+
+
+            ///////////////////////////////////////////////////////////////////////////// Variant 2 //////////////////////////////////////
+            string wordRemove = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(wordRemove))
+            {
+                text = text.Replace(wordRemove, "");
+
+            }
+            Console.WriteLine(text);
+
+
+        }
+    }
+}
