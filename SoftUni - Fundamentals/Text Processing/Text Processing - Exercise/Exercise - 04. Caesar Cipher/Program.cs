98aa99977b8865aa17c3a1071b29fa451f69fe22
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-02 00:08:59 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Text Processing/Text Processing - Exercise/Exercise - 04. Caesar Cipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise___04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string cipher = string.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char letter = text[i];
+                letter += (char)3;
+                cipher += letter;
+            }
+            Console.WriteLine(cipher);
+        }
+    }
+}
