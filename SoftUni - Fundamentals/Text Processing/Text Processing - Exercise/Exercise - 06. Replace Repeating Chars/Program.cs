7a219e30c88180d69d0c942c388b599f50178b99
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-02 00:10:23 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Text Processing/Text Processing - Exercise/Exercise - 06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise___06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToList();
+
+            for (int i = 0; i < text.Count - 1; i++)
+            {
+                char curr = text[i];
+                char next = text[i + 1];
+                if (curr == next)
+                {
+                    text.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join("", text));
+        }
+    }
+}
