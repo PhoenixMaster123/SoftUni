0c342b92c85af5ad4dbfd2dbb774ba1eb363cb2f
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-02 00:05:41 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Text Processing/Text Processing - Lab/04. Text Filter - Lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Text_Filter___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWord = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+            foreach (string word in bannedWord)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
