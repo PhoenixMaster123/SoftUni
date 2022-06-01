6f2ac86862ab32a7b444260a9e95c501b0920b63
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-02 00:09:51 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Text Processing/Text Processing - Exercise/Exercise - 05. Multiply Big Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+namespace Exercise___05._Multiply_Big_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          BigInteger number1 = BigInteger.Parse(Console.ReadLine());
+          BigInteger number2 = BigInteger.Parse(Console.ReadLine());
+          BigInteger result =  number1 * number2;
+          Console.WriteLine(result);
+        }
+    }
+}
