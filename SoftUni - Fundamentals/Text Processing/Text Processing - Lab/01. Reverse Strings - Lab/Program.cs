055feab7cdeedb4c51fd03855118bcff3c6c18a7
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-02 00:03:31 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Text Processing/Text Processing - Lab/01. Reverse Strings - Lab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Reverse_Strings___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();          
+            while (command!="end")
+            {
+
+                char[] wordArray = command.ToCharArray();
+                Array.Reverse(wordArray);
+                string reverseWord = new string(wordArray);
+                Console.WriteLine($"{command} = {reverseWord}");
+                command = Console.ReadLine();
+              
+            }
+           
+
+           
+          
+
+
+        }
+    }
+}
