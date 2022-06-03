8bc17efcd74b4f6db9a60117c3f5efd1731ee9e4
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-03 18:08:58 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Mid Exam/Mid Exam - 7 July 2021/01. Black Flag/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01._Black_Flag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int plunderForDay = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+            double dailyPlunder = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                dailyPlunder += plunderForDay;
+                if (i % 3 == 0)
+                {
+                  
+                    dailyPlunder += plunderForDay * 0.50;
+                }
+                if (i % 5 == 0)
+                {
+                   
+                    dailyPlunder -= dailyPlunder * 0.30;
+                }     
+            }
+            if(dailyPlunder>=expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {dailyPlunder:F2} plunder gained.");
+            }
+            else
+            {
+                Console.WriteLine($"Collected only {dailyPlunder / expectedPlunder * 100:F2}% of the plunder.");
+
+            }
+
+            
+        }
+    }
+}
