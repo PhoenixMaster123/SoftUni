265d275b9d1ac64e9460587bb2801094153daf30
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-03 18:03:01 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Mid Exam/Mid Exam - 07 April 2020/01. Counter-Strike/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            string command = string.Empty;
+
+            int wins = 0;
+
+            while ((command = Console.ReadLine()) != "End of battle")
+            {
+                int distance = int.Parse(command);
+
+                if (energy < distance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {energy} energy");
+                    return;
+                }
+
+                wins++;
+                energy -= distance;
+
+                if (wins % 3 == 0)
+                {
+                    energy += wins;
+                }
+            }
+
+            Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+        }
+    }
+}
