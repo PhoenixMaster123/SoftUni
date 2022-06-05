28e49e65a2141d033405e5bd61f41802b3bd3e27
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-06 00:35:54 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stack and Queues - Exercise/1. Basic Stack Operations - Exercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Basic_Stack_Operations___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int push = array[0];
+            int pop = array[1];
+            int num = array[2];
+            int[] number = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < push; i++)
+            {  
+                stack.Push(number[i]);
+            }
+            for (int i = 0; i < pop; i++)
+            {
+                stack.Pop();
+            }
+            if(stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if(stack.Contains(num))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+
+            
+
+        }
+    }
+}
