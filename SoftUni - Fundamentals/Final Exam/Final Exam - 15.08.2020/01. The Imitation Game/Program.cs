e42b4e298080d799707dbe54d816db5b12a9924b
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-05 08:28:03 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Final Exam/Final Exam - 15.08.2020/01. The Imitation Game/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01._The_Imitation_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string command = Console.ReadLine();
+            while (command != "Decode")
+            {
+                string[] token = command.Split('|');
+                string action = token[0];
+                if (action == "Move")
+                {
+                    int number = int.Parse(token[1]);
+                    string reverse = word.Substring(0, number);
+                    string secondReverse = word.Substring(number);
+                    word = secondReverse + reverse;
+                }
+                else if (action == "Insert")
+                {
+                    int add = int.Parse(token[1]);
+                    string letter = token[2];
+                    word = word.Insert(add, letter);
+                }
+                else if (action == "ChangeAll")
+                {
+                    char letter1 = char.Parse(token[1]);
+                    char letter2 = char.Parse(token[2]);
+                    word = word.Replace(letter1, letter2);
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"The decrypted message is: {word}");
+        }
+    }
+}
