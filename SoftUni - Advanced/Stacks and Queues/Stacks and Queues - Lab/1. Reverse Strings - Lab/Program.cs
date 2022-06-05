9d42bc7f22cad907b129b16b6286be9d58707762
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-05 20:59:43 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stacks and Queues - Lab/1. Reverse Strings - Lab/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////////////////////////// First Variant /////////////////////////////
+            //string input = Console.ReadLine();
+
+            //Stack<string> stack = new Stack<string>();
+
+            //for (int i = 0; i < input.Length; i++)
+            //{
+            //    stack.Push(String.Format($"{input[i]}"));
+            //}
+            //Console.WriteLine(string.Join("",stack));
+
+            ////////////////////////////////////////////////////////////// Second Variant ////////////////////////////
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (char ch in input)
+            {
+                stack.Push(ch);
+            }
+            while(stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
