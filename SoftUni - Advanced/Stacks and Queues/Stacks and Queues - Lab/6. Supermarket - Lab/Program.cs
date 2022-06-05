7d78cd525e5985b8259f39ec6228529fd1e4e3a0
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-06 00:33:46 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stacks and Queues - Lab/6. Supermarket - Lab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6_Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+            string input = null;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (names.Any())
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }
+                }
+                else
+                {
+                    names.Enqueue(input);
+                }
+            }
+
+            Console.WriteLine(names.Count + " people remaining.");
+        }
+    }
+}
+
