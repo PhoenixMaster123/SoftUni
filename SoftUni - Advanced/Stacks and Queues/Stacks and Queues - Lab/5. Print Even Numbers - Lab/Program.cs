dc721f95f1a0628e55a695b4b9208c9e61991369
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-06 00:33:27 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stacks and Queues - Lab/5. Print Even Numbers - Lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5_Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> evenNumbers = new Queue<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenNumbers.Enqueue(numbers[i]);
+                }
+            }
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
