369b4e9a132857f66788264fd3f2a768b715d478
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-06 00:33:02 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stacks and Queues - Lab/4. Matching Brackets - Lab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Stack<int> breckets = new Stack<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char ch = expression[i];
+                if(ch == '(')
+                {
+                    breckets.Push(i);
+                }
+                else if(ch == ')')
+                {
+                    int startIndex = breckets.Pop();
+                    int endIndex = i;
+                    string subexpression = expression.Substring(startIndex, endIndex - startIndex + 1); ;
+                    Console.WriteLine(subexpression);
+                }
+                        
+
+            
+            }
+        }
+    }
+}
