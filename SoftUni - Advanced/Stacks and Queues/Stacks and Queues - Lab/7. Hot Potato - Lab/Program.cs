8766b362dea430a74b53d0ecdb994da3e579356a
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-06 00:34:18 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stacks and Queues - Lab/7. Hot Potato - Lab/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(' ');
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(names); 
+          
+            while(queue.Count > 1)
+            {
+                for (int i = 1; i <= n-1; i++)
+                {
+                    string player = queue.Dequeue();
+                    queue.Enqueue(player);
+                }
+                string lostPlayer = queue.Dequeue();
+                Console.WriteLine($"Removed {lostPlayer}");
+            }
+            string lastPlayer = queue.Dequeue();
+            Console.WriteLine($"Last is {lastPlayer}");
+        }
+    }
+}
