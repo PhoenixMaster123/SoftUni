38c82e7fd818f0559655b359cd4e0a0d78ec2e40
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-06 00:32:24 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stacks and Queues - Lab/3. Simple Calculator - Lab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3_Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> calculator = new Stack<string>(Console.ReadLine().Split().Reverse().ToArray());
+
+            while (calculator.Count > 1)
+            {
+                int a = int.Parse(calculator.Pop());
+                string sign = calculator.Pop();
+                int b = int.Parse(calculator.Pop());
+
+                if (sign == "+")
+                {
+                    calculator.Push((a + b).ToString());
+                }
+                else
+                {
+                    calculator.Push((a - b).ToString());
+                }
+            }
+
+            Console.WriteLine(calculator.Pop());
+        }
+    }
+}
