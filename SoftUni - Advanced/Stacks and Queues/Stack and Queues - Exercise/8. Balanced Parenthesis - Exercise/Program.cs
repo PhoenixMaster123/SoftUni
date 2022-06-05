7b0871689b1891d4a9989b552516a3694de020d6
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-06 00:41:11 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stack and Queues - Exercise/8. Balanced Parenthesis - Exercise/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BalancedParenthesis
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char bracket = input[i];
+
+                if (bracket == '(' || bracket == '[' || bracket == '{')
+                {
+                    stack.Push(bracket);
+                }
+                else if (bracket == ')' && stack.Any() || bracket == ']' && stack.Any() || bracket == '}' && stack.Any())
+
+                {
+
+                    char currentBracket = input[i];
+                    char stackBracket = stack.Pop();
+
+                    if (stackBracket == '(' && currentBracket == ')')
+                    {
+                        continue;
+                    }
+                    else if (stackBracket == '[' && currentBracket == ']')
+                    {
+                        continue;
+                    }
+                    else if (stackBracket == '{' && currentBracket == '}')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}
