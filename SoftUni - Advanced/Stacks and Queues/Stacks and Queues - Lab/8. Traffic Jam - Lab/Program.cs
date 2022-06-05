f0c5296e1a4e8db0df0efd6b345de7b3502d1599
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-06 00:34:41 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stacks and Queues - Lab/8. Traffic Jam - Lab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            Queue<string> queue = new Queue<string>();
+            while(true)
+            {
+                string command = Console.ReadLine();
+                if(command=="green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if(queue.Count>0)
+                        {
+                            string car = queue.Dequeue();
+                            Console.WriteLine($"{car} passed!");
+                            count++;
+                        }
+                    }
+                }
+                else if(command=="end")
+                {
+                    Console.WriteLine($"{count} cars passed the crossroads.");
+                    break;
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+        }
+    }
+}
