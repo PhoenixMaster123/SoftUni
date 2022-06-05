938d89e96ebf300a89a4b47884840aa4dfe288d8
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-06 00:38:18 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Stacks and Queues/Stack and Queues - Exercise/5. Fashion Boutique - Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5_Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] allClothesInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> allClothes = new Stack<int>(allClothesInput);
+            int boxCapacity = int.Parse(Console.ReadLine());
+            int currentRackCapacity = boxCapacity;
+            int racksCount = 1;
+
+            while (allClothes.Any())
+            {
+                int clothes = allClothes.Pop();
+                currentRackCapacity -= clothes;
+                if (currentRackCapacity < 0)
+                {
+                    racksCount++;
+                    currentRackCapacity = boxCapacity - clothes;
+                }
+            }
+
+            Console.WriteLine(racksCount);
+        }
+    }
+}
