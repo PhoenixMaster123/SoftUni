2a84433e75e598532f80ee9337a3c25122d4c022
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-07 23:24:56 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Lab/03. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;           
+            while(number>sum)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;               
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
