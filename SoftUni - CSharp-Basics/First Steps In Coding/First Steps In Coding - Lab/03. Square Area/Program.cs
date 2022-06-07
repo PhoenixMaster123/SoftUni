b97af490eb5fd58f1fba6a1aa12bffeb85cc34db
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-07 23:26:50 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Lab/03. Square Area/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03._Square_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int result = size * size;
+            Console.WriteLine(result);
+        }
+    }
+}
