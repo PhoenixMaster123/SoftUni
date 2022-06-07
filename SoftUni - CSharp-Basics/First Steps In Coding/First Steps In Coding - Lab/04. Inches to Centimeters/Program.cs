ff639212f30ed70601c4bbd955c9f1993cb61dd8
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-07 23:28:06 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Lab/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04._Inches_to_Centimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double inches = number * 2.54;
+            Console.WriteLine(inches);
+        }
+    }
+}
