8bb6752f29b515b6e56469cd621a47eb0a0ebc1e
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-07 23:35:33 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Lab/09. Yard Greening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _09._Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double quadratmeters = double.Parse(Console.ReadLine());
+            double persent = 0.18;
+            quadratmeters = quadratmeters * 7.61;
+            persent = persent * quadratmeters;
+            double result = quadratmeters - persent;
+            Console.WriteLine($"The final price is: {result} lv.");
+            Console.WriteLine($"The discount is: {persent} lv.");
+        }
+    }
+}
