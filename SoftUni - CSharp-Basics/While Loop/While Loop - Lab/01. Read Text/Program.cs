7f3f044e562e9c0de22887c8f763a111d22dd2a7
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-07 23:22:53 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Lab/01. Read Text/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+            while (names != "Stop") 
+            {               
+                Console.WriteLine(names);
+                names = Console.ReadLine();
+            }
+            
+        }
+    }
+}
