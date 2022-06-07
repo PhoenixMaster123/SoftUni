e4a821b46adfb58a60a0871f0a3bc03a7c0f6142
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-07 23:31:27 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Lab/07. Projects Creation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string architect = Console.ReadLine();
+            int projects = int.Parse(Console.ReadLine());
+            int hours = projects * 3;
+            Console.WriteLine($"The architect {architect} will need {hours} hours to complete {projects} project/s.");
+        }
+    }
+}
