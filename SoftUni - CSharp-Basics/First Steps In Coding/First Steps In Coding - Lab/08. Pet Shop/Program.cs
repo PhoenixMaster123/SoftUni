34be7a7839cf3e7e487e441c5827ce0b89c69cd3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-07 23:32:35 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Lab/08. Pet Shop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08._Pet_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int empty = int.Parse(Console.ReadLine());
+
+            double fooddogs = dogs * 2.50;
+            double foodempty = empty * 4;
+            double result = fooddogs + foodempty;
+            Console.WriteLine($"{result} lv.");
+        }
+    }
+}
