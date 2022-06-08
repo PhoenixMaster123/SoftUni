2186b1b3624e25367123e97aeb327dedd2ec26ba
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-08 23:45:48 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements/Conditional Statements - Lab/02. Greater Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Greater_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int secoundnumber = int.Parse(Console.ReadLine());
+
+            if (number > secoundnumber)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine(secoundnumber);
+            }
+
+        }
+    }
+}
