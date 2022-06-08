e39551c9cd7c56138933b44bd9416181a329c155
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-08 23:38:08 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Exercise/08. Fish Tank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _17._Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double high = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double result = lenght * width * high;
+            result = result * 0.001;
+            percent = percent * 0.01;
+            result = result * (1 - percent);
+            Console.WriteLine(result);
+        }
+    }
+}
