b6347588908d8fe1a00d353bde066eb10254670e
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-08 23:31:42 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _12._Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount = double.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            double yearpercent = double.Parse(Console.ReadLine());
+            //double result = amount + month*((amount * yearpercent) / 12);
+            double result = amount * (yearpercent/100);
+            double resultformonth = result / 12;
+            double end = amount + (month * resultformonth);
+            Console.WriteLine(end);
+        }
+    }
+}
