9cd235afeb8440caa90f2ca1e732742daf9cdf1a
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-08 23:33:03 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _13._Vacation_Books_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lists = int.Parse(Console.ReadLine());
+            double listsforhour = double.Parse(Console.ReadLine());
+            int daysofbooks = int.Parse(Console.ReadLine());
+
+            double hours = lists / listsforhour;
+            hours = hours / daysofbooks;
+            Console.WriteLine(hours);
+        }
+    }
+}
