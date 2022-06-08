8e0c79efb573d599fa92e66edd9ead04731e1eee
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-08 23:34:06 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Exercise/05. Birthday Party/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _14._Birthday_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double room = double.Parse(Console.ReadLine());
+            double torte = room  * 0.20;
+            double drinks = torte - torte * 0.45;
+            double animator = room * 1 / 3 ;
+            Console.WriteLine(room+torte+drinks+animator);
+        }
+    }
+}
