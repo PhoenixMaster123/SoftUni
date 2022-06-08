c11b53b096c964af803242c8a9b4e73ed80b67a3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-08 23:36:13 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Exercise/06. Charity Campaign/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _15._Charity_Campaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int personal = int.Parse(Console.ReadLine());
+            int countcakes = int.Parse(Console.ReadLine());
+            int gofretscount = int.Parse(Console.ReadLine());
+            int pancakescount = int.Parse(Console.ReadLine());          
+
+            double pricecakes = countcakes * 45;
+            double pricegofrets = gofretscount * 5.80;
+            double pricepancakes = pancakescount * 3.20;
+
+            double result = (pricecakes + pricegofrets + pricepancakes) * personal;
+            result = result * days;
+            result = result - result * 0.125;
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
