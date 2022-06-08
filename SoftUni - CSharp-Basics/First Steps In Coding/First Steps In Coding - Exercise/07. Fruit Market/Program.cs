c08d26ef1f3044f72890cdb7848893c5b2ceb9f6
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-08 23:37:03 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps In Coding - Exercise/07. Fruit Market/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _16._Fruit_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double erdberreprice = double.Parse(Console.ReadLine());
+            double banans = double.Parse(Console.ReadLine());
+            double oranges = double.Parse(Console.ReadLine());
+            double berry = double.Parse(Console.ReadLine());
+            double strawberry = double.Parse(Console.ReadLine());
+
+           double  berryprice = erdberreprice / 2;
+           double orangesprice = berryprice - 0.4* berryprice ;
+            double banansprice = berryprice - 0.8 * berryprice ;
+
+            berryprice = berryprice * berry;
+            orangesprice = oranges * orangesprice;
+            banansprice = banansprice * banans;
+            strawberry = strawberry * erdberreprice;
+            double result = (berryprice + orangesprice + banansprice + strawberry);
+            Console.WriteLine(Math.Round(result, 2));
+
+
+
+
+
+        }
+    }
+}
