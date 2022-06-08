57993e88e690da17629a2353df79d0627a8cf132
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-08 23:44:23 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements/Conditional Statements - Lab/01. Excellent Result/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = double.Parse(Console.ReadLine());
+            if(result>=5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+           
+        }
+    }
+}
