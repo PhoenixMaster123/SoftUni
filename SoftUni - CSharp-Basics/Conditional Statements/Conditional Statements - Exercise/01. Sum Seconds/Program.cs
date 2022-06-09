1ab621dd2c976298ce1991c9d0f2193e3d741001
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-09 23:47:16 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements/Conditional Statements - Exercise/01. Sum Seconds/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sum_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int totalTime = firstTime + secondTime + thirdTime;
+            int minutes = 0;
+            int seconds = 0;
+            if(totalTime>=120)
+            {
+                minutes = 2;
+                seconds = totalTime - 120;
+            }
+            else if(totalTime>=60)
+            {
+                minutes = 1;
+                seconds = totalTime - 60;
+            }
+            else
+            {
+                seconds = totalTime;
+            }
+            Console.WriteLine($"{minutes}:{seconds:d2}");
+        }
+    }
+}
