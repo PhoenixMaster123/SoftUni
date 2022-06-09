2d218d1ed88d2d5a4681da4335e85c619dd04182
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-09 23:52:50 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements/Conditional Statements - Exercise/07. Shopping/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double peterbuget = double.Parse(Console.ReadLine());
+            int videocarts = int.Parse(Console.ReadLine());
+            int cpu = int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+
+            double videocartsprice = videocarts * 250;
+            double totalpricecpu = videocartsprice * 0.35;
+            double totalrammemoryprice = videocartsprice * 0.10;
+
+            double TotalMoney = videocartsprice + totalpricecpu * cpu + totalrammemoryprice * ram;
+
+            if(videocarts>cpu)
+            {
+                TotalMoney *= 0.85;
+            }
+            double difference = Math.Abs(TotalMoney - peterbuget);
+            if(peterbuget>=TotalMoney)
+            {
+                Console.WriteLine($"You have {difference:F2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {difference:F2} leva more!");
+            }
+        }
+    }
+}
