1b64e4f4da0f7f0c259a0548e942d079a034eb48
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-09 23:49:56 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements/Conditional Statements - Exercise/03. Time15 Minutes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Time15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minuts = int.Parse(Console.ReadLine());
+            int total = hours + minuts;            
+            minuts += 15;
+
+            if(minuts>=60)
+            {
+                minuts -= 60;
+                hours += 1;
+            }
+            if(hours>=24)
+            {
+                hours = 0;
+            }
+
+            Console.WriteLine($"{hours}:{minuts:d2}");
+        }
+    }
+}
+        
+    
+
