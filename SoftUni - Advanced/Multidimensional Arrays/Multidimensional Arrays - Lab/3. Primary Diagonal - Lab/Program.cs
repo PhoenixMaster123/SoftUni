4d8dccb1fa9c7b5b39a1a025ce6f7bdae13c3b45
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-09 23:37:22 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Multidimensional Arrays/Multidimensional Arrays - Lab/3. Primary Diagonal - Lab/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            for (int row = 0; row < size; row++)
+            {
+                int[] line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row,col] = line[col];
+                }
+            }
+            int sum = 0;
+            for (int row = 0;row < size; row++)
+            {
+               
+                for (int col = 0;col < size; col++)
+                {
+
+                    sum += matrix[row, col];
+                    row++;
+                    
+                }
+               
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
