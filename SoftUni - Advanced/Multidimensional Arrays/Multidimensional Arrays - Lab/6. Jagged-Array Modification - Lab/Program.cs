1e8832cd2fa54f51425b949597e85773deb5c06d
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-10 19:52:41 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Multidimensional Arrays/Multidimensional Arrays - Lab/6. Jagged-Array Modification - Lab/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Modification___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCount = int.Parse(Console.ReadLine());
+            int[][] jagged = new int[rowsCount][];
+            //////////////////////////////////////////////////////////// First Variant //////////////////////////////
+            //for (int row = 0; row < rowsCount; row++)
+            //{
+            //    jagged[row] = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            //}
+            ////////////////////////////////////////////////////////// Second Variant //////////////////////////////
+            for (int row = 0; row < rowsCount; row++)
+            {
+                int[] columns = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                jagged[row] = columns;
+            }
+            string command = Console.ReadLine();
+            while (command != "END")
+            { 
+                string[] token = command.Split(' ').ToArray();
+                string action = token[0];
+                if(action.Equals("Add"))
+                {
+                    int row = int.Parse(token[1]);
+                    int col = int.Parse(token[2]);
+                    int value = int.Parse(token[3]);
+
+
+                    if (row >= 0 && col >= 0 &&
+                        row < rowsCount && col < rowsCount)
+                    {
+                        jagged[row][col] += value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+                    
+                   
+                }
+                else if(action.Equals("Subtract"))
+                {
+                    int row = int.Parse(token[1]);
+                    int col = int.Parse(token[2]);
+                    int value = int.Parse(token[3]);
+
+                    if (row >= 0 && col >= 0 &&
+                        row < rowsCount && col < rowsCount)
+                    {
+                        jagged[row][col] -= value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates");
+                    }
+
+                }
+                command = Console.ReadLine();
+            }
+            /////////////////////////////////////////////// First Variant ///////////////////////////////////////
+            foreach(int[] row in jagged)
+            {
+                Console.WriteLine(string.Join(" ", row)); 
+            }
+            ///////////////////////////////////////////// Second Variant //////////////////////////////////////
+            //for (int row = 0; row < jagged.Length; row++)
+            //{
+               
+            //        Console.WriteLine(string.Join(" ",jagged[row]));
+       
+            //}
+        }
+    }
+}
