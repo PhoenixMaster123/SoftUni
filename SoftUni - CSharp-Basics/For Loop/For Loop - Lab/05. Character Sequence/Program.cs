a789815a243e9a119e515a159eb5128463a95987
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-10 23:48:20 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For Loop - Lab/05. Character Sequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+          for(int i =0;i<text.Length;i++)
+            {
+                char letter = text[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
