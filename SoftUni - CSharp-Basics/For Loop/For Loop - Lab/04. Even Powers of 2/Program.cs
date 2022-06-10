69ee5fb88553b2adea25b4f824b13e0e094860a3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-10 23:46:50 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For Loop - Lab/04. Even Powers of 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Powers_of_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            for(int count = 0;count<=n;count+=2)
+            {
+                Console.WriteLine(num);
+                num = num * 2 * 2;
+            }
+
+        }
+    }
+}
