c76a6fc7399cd41c0bf23ac96bf2a0ee781c4b2b
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-10 23:50:36 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For Loop - Lab/08. Number sequence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int MaxValue =int.MinValue;
+            int MinValue = int.MaxValue;
+            for(int i=0;i<n;i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if(num>MaxValue)
+                {
+                    MaxValue = num;
+                }
+                if(num<MinValue)
+                {
+                    MinValue = num;
+                }
+
+            }
+            Console.WriteLine($"Max number: {MaxValue}");
+            Console.WriteLine($"Min number: {MinValue}");
+
+        }
+    }
+}
