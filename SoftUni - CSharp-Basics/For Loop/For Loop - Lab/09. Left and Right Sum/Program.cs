711f3f62b3154aeb30f1873f29b3dd94a4a1882a
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-10 23:51:06 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For Loop - Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sumleft = 0;
+            int sumright = 0;
+            for(int i=0;i<number;i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+                sumleft += current;
+ 
+            }
+            for(int i=0;i<number;i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+                sumright += current;
+            }
+            if(sumleft==sumright)
+            {
+                Console.WriteLine($"Yes, sum = {sumleft}");
+            }
+            else if(sumleft!=sumright)
+            {
+                int diff = sumleft - sumright;
+                Console.WriteLine($"No, diff = {Math.Abs(diff)}");
+            }
+           
+        }
+    }
+}
