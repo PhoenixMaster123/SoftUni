20fa7c7f3b56285e47cee419895d7f9113d7e3d1
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-10 23:46:20 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For Loop - Lab/03. Numbers 1N with Step 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_1N_with_Step_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for(int count = 1;count<=num;count+=3)
+            {
+
+                Console.WriteLine(count);
+                    
+            }
+        }
+    }
+}
