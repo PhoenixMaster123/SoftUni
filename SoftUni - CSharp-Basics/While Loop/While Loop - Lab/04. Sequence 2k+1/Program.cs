9ed4bf38f61162123119c7af47c21a5f6b1b3aef
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-11 22:44:28 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Lab/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sequence_2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int number = 1;
+            while(n>=number)
+            {
+                Console.WriteLine(number);
+                number = number * 2 + 1;
+                
+              
+            }
+            
+        }
+    }
+}
