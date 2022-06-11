c44ee880411494db63fed89380ad0331d229acf4
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-11 22:48:26 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.MaxValue;
+            string num = Console.ReadLine();
+            while (num != "Stop")
+            {
+                int number = int.Parse(num);
+                if (min > number)
+                {
+                    min = number;
+
+                }
+                num = Console.ReadLine();
+
+            }
+            Console.WriteLine(min);
+        }
+    }
+    }
+
