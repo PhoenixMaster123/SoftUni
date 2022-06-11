af73e1f307465ab6e3bd07ce6f2a40566a902a71
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-11 22:45:21 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string balance = Console.ReadLine();
+            double sum = 0;
+            while (balance != "NoMoreMoney")
+            {
+                double money = double.Parse(balance);               
+                if (money >= 0)
+                {
+                    Console.WriteLine($"Increase: {money:F2}");
+                    sum += money;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                balance = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {sum:F2}");
+                
+
+            
+        }
+    }
+}
