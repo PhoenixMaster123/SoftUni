7ca02d6c0b22ca75784e746ff4b2429b0e9957f0
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-11 22:38:47 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For Loop - Exercise/02. Half Sum Element/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int value = int.MinValue;
+            int sum = 0;
+            for(int i=0;i<n;i++)
+            {
+                int allnumbers = int.Parse(Console.ReadLine());
+                sum += allnumbers;
+
+                if (allnumbers>value)
+                {
+                    value = allnumbers;
+                    
+                }
+            }
+            int without = sum - value;
+            if(without==value)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {value}");
+            }
+            else
+            {
+                int diff = Math.Abs(value - without);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+
+            }
+        }
+    }
+}
