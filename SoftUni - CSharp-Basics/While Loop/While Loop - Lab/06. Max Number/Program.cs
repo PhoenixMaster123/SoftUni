2ef0f11e62db9602a41a55d606a76ebf3d1ff7bc
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-11 22:46:36 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MinValue;
+            string num = Console.ReadLine();
+            while(num!="Stop")
+            {
+                int number = int.Parse(num);             
+                if(max<number)
+                {
+                    max = number;
+                   
+                }
+                num = Console.ReadLine();
+                
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
