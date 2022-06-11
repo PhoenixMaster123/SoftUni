3c2a51bcc69972ce942c4b1f258b6d37ecfc0b34
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-11 18:19:38 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Multidimensional Arrays/Multidimensional Arrays - Exercise/2. Squares in Matrix - Exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rowCount =input[0];
+            int colCount =input[1];
+            string[,] matrix = new string[rowCount, colCount];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] text = Console.ReadLine().Split(" ");
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = text[col];
+                }
+            }
+            int count = 0;
+            for (int row = 0; row < rowCount - 1; row++)
+            {
+                for (int col = 0; col < colCount - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row,col]==matrix[row + 1, col] && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+                Console.WriteLine(count);
+        }
+    }
+}
