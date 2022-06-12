12f34bd1fb7956f0ae5d7132f741067ca5e8e469
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-12 23:38:52 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Exercise/04. Walking/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  total = 0;
+            while (total < 10000)
+            {
+                string steps = Console.ReadLine();
+                if (steps == "Going home")
+                {
+                    int stepsLeft = int.Parse(Console.ReadLine());
+                    total += stepsLeft;
+                    break;
+                }
+                int currentsteps = int.Parse(steps);
+                total += currentsteps;                
+            }
+            if (total >= 10000)
+            {
+                Console.WriteLine($"Goal reached! Good job!");
+                int goal = total - 10000;
+                Console.WriteLine($"{goal} steps over the goal!");
+            }
+            else
+            {
+                int diff = 10000 - total;
+                Console.WriteLine($"{diff} more steps to reach goal.");
+            }
+
+        }
+    }
+}
