e507a0da5f4c07fd66ad0dcd1ee30cb0959475b2
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-13 00:40:53 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Nested Loops/Nested Loops - Lab/03. Combinations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for(int i=0;i<=number;i++)
+            {
+                for(int j=0;j<=number;j++)
+                {
+                    for(int k=0;k<=number;k++)
+                    {                    
+                        if (i+j+k== number)
+                        {
+                            counter++;   
+                        }                       
+                    }                   
+                    
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
