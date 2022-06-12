0760135d9d20865031fed2e51aaf2fd101cae6fe
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-12 23:44:35 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Exercise/06. Cake/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            int cakePieces = lenght * width;
+
+            while(cakePieces>0)
+            {
+                string input = Console.ReadLine();
+                if(input=="STOP")
+                {
+                    break;
+                }
+                int PieceOfCake = int.Parse(input);
+                cakePieces -= PieceOfCake;
+            }
+            if(cakePieces>=0)
+            {
+                Console.WriteLine($"{cakePieces} pieces are left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(cakePieces)} pieces more.");
+            }
+        }
+    }
+}
