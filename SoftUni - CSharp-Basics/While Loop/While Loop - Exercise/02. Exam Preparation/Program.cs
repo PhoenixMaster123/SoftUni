ee8b1b5ce3c70ab94f0aeaac314a23f8d9c22123
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-12 23:37:26 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Exercise/02. Exam Preparation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exam_Preparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int failnotes = int.Parse(Console.ReadLine());
+            int countfail = 0;
+            string problem = Console.ReadLine();
+            string lastproblem = "";
+            int gradeSum = 0;
+            int allgrades = 0;
+            while(problem!="Enough")
+            {
+                int currentGrade = int.Parse(Console.ReadLine());
+                gradeSum++;
+                allgrades += currentGrade;               
+                if(currentGrade<=4)
+                {
+                    countfail++;
+                    if(countfail==failnotes)
+                    {
+                        break;
+                    }
+                }
+                lastproblem = problem;
+                problem = Console.ReadLine();
+            }
+            if(problem=="Enough")
+            {
+                double averagescore = 1.0 * allgrades / gradeSum;
+                Console.WriteLine($"Average score: {averagescore:F2}");
+                Console.WriteLine($"Number of problems: {gradeSum}");
+                Console.WriteLine($"Last problem: {lastproblem}");
+            }
+            else
+            {
+                Console.WriteLine($"You need a break, {countfail} poor grades.");
+            }
+
+
+        }
+    }
+}
