654944eaed37d97636784e16c144b8d90fcb6347
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-13 00:55:16 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Nested Loops/Nested Loops - Lab/01. Clock/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 23; i++)
+            {
+                for (int n = 0; n <= 59; n++)
+                {
+                    Console.WriteLine($"{i}:{n}");
+                }
+            }
+        }
+    }
+}
