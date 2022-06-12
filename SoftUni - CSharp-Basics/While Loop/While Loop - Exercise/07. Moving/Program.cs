7eddda7830d054b1141ff94b955270a017cbd75d
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-12 23:45:07 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Exercise/07. Moving/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int freespace = width * lenght * height;
+
+            int cube = 0;
+            string command = Console.ReadLine();
+            while (command != "Done") 
+            {
+                int current = int.Parse(command);
+                cube += current;
+                if(freespace<cube)
+                {
+                    int needed = cube - freespace;
+                    Console.WriteLine($"No more free space! You need {needed} Cubic meters more.");
+                    break;
+                }
+                command = Console.ReadLine();
+
+            }
+            if(command=="Done")
+            {
+                int cubic = freespace - cube;
+                Console.WriteLine($"{cubic} Cubic meters left.");
+            }
+        }
+    }
+}
