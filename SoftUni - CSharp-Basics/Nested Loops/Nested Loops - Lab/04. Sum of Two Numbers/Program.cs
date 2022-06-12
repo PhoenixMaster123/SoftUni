6c028eea9b7e3b21ab0a875caa652b19cf14c38a
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-13 00:34:14 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Nested Loops/Nested Loops - Lab/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = int.Parse(Console.ReadLine());
+            int secondnumber = int.Parse(Console.ReadLine());            
+            int magicnumber = int.Parse(Console.ReadLine());
+            int counter = 0;           
+            bool flag = false;
+            for (int i = firstnumber; i <= secondnumber; i++)
+            {
+                for (int j = firstnumber; j <= secondnumber; j++)
+                {
+                    counter++;
+                    int sum = i + j;
+                    if (sum == magicnumber)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {magicnumber})");
+                        flag = true;
+                        break;
+                        // return;
+                    }
+
+                }
+                if (flag)
+                {
+                    break;
+                }
+            }
+                if (!flag)
+                { Console.WriteLine($"{counter} combinations - neither equals {magicnumber}"); }             
+                       
+        }
+
+    }
+}
