643bc8bd1bdd0ea7ddd9452a9085a4ed7fcc6fb3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-12 23:36:29 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/While Loop/While Loop - Exercise/01. Old Books/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string search = Console.ReadLine();
+            int count = 0;
+            string book = Console.ReadLine();
+            while(book!="No More Books")
+            {
+                if(book==search)
+                {
+                    Console.WriteLine($"You checked {count} books and found it.");
+                    break;
+                }
+                count++;
+                book = Console.ReadLine();
+            }
+            if (book == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {count} books.");
+            }
+        }
+    }
+}
