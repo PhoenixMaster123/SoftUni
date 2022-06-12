ccfb7e1da93da6f625b28e0466772ef0374ef19b
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-12 23:50:39 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Nested Loops/Nested Loops - Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetNum = int.Parse(Console.ReadLine());
+            int row = 1;
+            int currentNum = 1;
+            bool isEqual = false;
+            while(!isEqual)
+            {
+                for(int i=1;i<=row;i++)
+                {
+                    Console.Write($"{currentNum} ");
+                    currentNum++;
+                    if(currentNum>targetNum)
+                    {
+                        isEqual = true;
+                        break;
+                    }
+
+                }
+                Console.WriteLine();
+                row++;
+            }
+        }
+    }
+}
