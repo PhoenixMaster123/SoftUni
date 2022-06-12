6eace41af7fa7f55870cd9e8a3e2e2b31d487013
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-13 00:08:54 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Nested Loops/Nested Loops - Exercise/04. Train The Trainers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string presentation = Console.ReadLine();
+            double average = 0;
+            int gradecount = 0;
+            double allgrade = 0;
+            while(presentation!="Finish")
+            {
+                double sumGrade = 0;
+                for(int i=1;i<=n;i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    sumGrade += grade;
+                    gradecount++;
+                    allgrade += grade;
+
+                }
+                double averagepresentation = sumGrade / n;
+                Console.WriteLine($"{presentation} - {averagepresentation:F2}.");
+                presentation = Console.ReadLine();
+            }
+            average = allgrade / gradecount;
+            Console.WriteLine($"Student's final assessment is {average:F2}.");
+        }
+    }
+}
