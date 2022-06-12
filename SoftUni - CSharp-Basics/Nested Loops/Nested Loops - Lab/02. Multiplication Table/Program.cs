9c891eb3305b203e72b6ec5a0db0325cd752afff
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-13 00:36:07 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Nested Loops/Nested Loops - Lab/02. Multiplication Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = int.Parse(Console.ReadLine());
+            for(int i=1;i<=10;i++)
+            {
+                for(int j =1; j<=10;j++)
+                {
+                    int number = i * j;
+                    Console.WriteLine($"{i} * {j} = {number}");
+                }
+               
+            }
+        }
+    }
+}
