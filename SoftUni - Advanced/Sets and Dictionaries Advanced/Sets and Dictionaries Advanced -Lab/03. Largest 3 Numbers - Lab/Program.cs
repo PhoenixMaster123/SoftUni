56a13766cf36ece4faa67659bb33a4b59195a309
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-13 12:33:54 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced -Lab/03. Largest 3 Numbers - Lab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////////////////////////////////////////////////////////////////////// First Variant /////////////////////////////////
+
+            //List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(x => x).ToList();
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    if(i<nums.Count)
+            //    {
+            //        Console.Write($"{nums[i]} " );
+            //    }
+            //}
+
+            ////////////////////////////////////////////////////////////////////// Second Variant /////////////////////////////////
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(x => x).ToList().Take(3);
+            Console.WriteLine(string.Join(" ",nums));
+
+
+        }
+    }
+}
