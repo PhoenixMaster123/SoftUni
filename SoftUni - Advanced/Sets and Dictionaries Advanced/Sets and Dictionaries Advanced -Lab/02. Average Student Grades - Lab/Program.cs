182b21ac7119d7e2a8895808f0a88249b6938e4c
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-13 13:45:53 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced -Lab/02. Average Student Grades - Lab/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int GradesCount = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentInfo = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < GradesCount; i++)
+            {
+                string[] scholars = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string names = scholars[0];
+                decimal grade = Convert.ToDecimal(scholars[1]);
+
+                if (!studentInfo.ContainsKey(names))
+                {
+                    studentInfo.Add(names, new List<decimal>() { grade });
+                }
+                else
+                {
+                    studentInfo[names].Add(grade);
+                }
+            }
+
+            foreach (var student in studentInfo)
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(' ', student.Value.Select(grade => grade.ToString("F2")))} (avg: {student.Value.Average():f2})");
+            }
+        }
+    }
+}
