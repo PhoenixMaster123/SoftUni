a0894bc515767c1b67aeaf18047d6ff2c78b295e
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-13 12:33:07 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced -Lab/01. Count Same Values in Array - Lab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+           var countNumbers = new Dictionary<double, int>();
+            foreach (int num in numbers)
+            {
+                if(countNumbers.ContainsKey(num))
+                {
+                    countNumbers[num]++;
+                }
+                else
+                {
+                    countNumbers[num] = 1;
+                }
+            }
+            foreach(var pair in countNumbers)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} times");
+            }
+           
+        }
+    }
+}
