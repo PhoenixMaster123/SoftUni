f955757207dd49001754e21d8405d17b51965e35
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-14 23:44:55 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps in Coding - More Exercises/01. Trapeziod Area/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Trapeziod_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b1 = double.Parse(Console.ReadLine());
+            double b2 = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double Area = (b1 + b2) * h / 2;
+            Console.WriteLine("{0:0.00}", Area);
+
+        }
+    }
+}
