3bf736b28d929788d6afb49c872be56b6c1f9f9a
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-14 19:30:23 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Exercise/04. Even Times - Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Even_Times___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int input = int.Parse(Console.ReadLine());
+            Dictionary<int,int> dictionaryEvenNumbers = new Dictionary<int, int>(); 
+            for (int i = 0; i < input; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if(!dictionaryEvenNumbers.ContainsKey(num))
+                {
+                    dictionaryEvenNumbers.Add(num, 1); ;
+                }
+                else
+                {
+                    dictionaryEvenNumbers[num]++;
+                }
+            }
+            Console.WriteLine(dictionaryEvenNumbers.First(entry => entry.Value % 2 == 0).Key); 
+        }
+    }
+}
