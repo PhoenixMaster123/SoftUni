fb04a2883e63c882aa6cb98d7d9b5854ff01bc89
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-14 12:04:19 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced -Lab/06. Record Unique Names - Lab/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /////////////////////////////////////////////////////////////////// First Variant - slow! //////////////////////
+            //int namesCount = int.Parse(Console.ReadLine());
+            // List<string> listNames = new List<string>();
+
+            // for (int i = 0; i < namesCount; i++)
+            // {
+            //     string name = Console.ReadLine();
+            //     if(!listNames.Contains(name))
+            //     {
+            //         listNames.Add(name);
+            //     }
+            //     else
+            //     {
+            //         continue;
+            //     }
+            // }
+            // foreach (string name in listNames)
+            // {
+            //     Console.WriteLine(name);
+            // }
+
+            /////////////////////////////////////////////////////////////////// Second Variant - fast! //////////////////////
+            
+            int namesCount = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+            for (int i = 0; i < namesCount; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+
+            }
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
