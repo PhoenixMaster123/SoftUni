69bbd03cd91774f034b20a0f7f757b0182d4f6ea
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-15 00:08:36 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps in Coding - More Exercises/06. Fishland/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Fishland
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double mackerel = double.Parse(Console.ReadLine());
+            double sprat = double.Parse(Console.ReadLine());
+            double bonito = double.Parse(Console.ReadLine());
+            double scad = double.Parse(Console.ReadLine());
+            int mussels = int.Parse(Console.ReadLine());
+
+
+
+            double mackerelprice = mackerel + mackerel * 0.6;
+            double bonitopriceresult = mackerelprice * bonito;
+            double spratprice = sprat + 0.8 * sprat;
+            double scadpriceresult = scad * spratprice;
+            double musselsprice = mussels * 7.50;
+
+            double result = bonitopriceresult + scadpriceresult + musselsprice;
+            Console.WriteLine("{0:0.00}", result);
+
+        }
+    }
+}
