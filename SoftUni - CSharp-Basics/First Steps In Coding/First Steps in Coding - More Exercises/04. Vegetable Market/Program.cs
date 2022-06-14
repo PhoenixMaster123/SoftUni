9e0e6ae68741545b2343ce3c0e826f3454d798d5
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-15 00:07:28 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps in Coding - More Exercises/04. Vegetable Market/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Vegetable_Market
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegetables = double.Parse(Console.ReadLine());
+            double fruits = double.Parse(Console.ReadLine());
+            int kilogramvegetables = int.Parse(Console.ReadLine());
+            int kilogramfruits = int.Parse(Console.ReadLine());
+
+            double vegetables1 = vegetables * kilogramvegetables;
+            double fruits1 = fruits * kilogramfruits;
+
+            double result = vegetables1 + fruits1;
+            result = result / 1.94;
+            Console.WriteLine("{0:0.00}", result);
+        }
+    }
+}
