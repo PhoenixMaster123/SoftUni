d0469b4164d5ee9ac154d9045c5083c7fc024057
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-14 12:04:51 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced -Lab/07. Parking Lot - Lab/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Parking_Lot___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string command = Console.ReadLine();
+            HashSet<string> carNames = new HashSet<string>();
+            while (command != "END") 
+            {
+                string[] token = command.Split(',');
+                string action = token[0];
+                string carNumber = token[1];
+                if(action.Equals("IN"))
+                {
+                    carNames.Add(carNumber);
+                }
+                else if(action.Equals("OUT"))
+                {
+                    carNames.Remove(carNumber);
+                }
+                command = Console.ReadLine();
+            }
+            if(carNames.Any())
+            {
+                foreach (string carName in carNames)
+                {
+                    Console.WriteLine(carName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            
+        }
+    }
+}
