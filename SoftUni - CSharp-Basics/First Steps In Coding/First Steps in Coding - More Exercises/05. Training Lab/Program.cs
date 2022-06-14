f3d88e7ed28cff652e4364352d99d2366ff2898f
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-15 00:08:01 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps in Coding - More Exercises/05. Training Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05._Training_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double h = double.Parse(Console.ReadLine());
+
+            double w = double.Parse(Console.ReadLine());
+            h *= 100;
+            w *= 100;
+            double h1 = Math.Floor(h / 120);
+            double w1 = Math.Floor((w - 100) / 70);
+
+            double result = (w1 * h1) - 3;
+            Console.WriteLine(result);
+        }
+    }
+}
