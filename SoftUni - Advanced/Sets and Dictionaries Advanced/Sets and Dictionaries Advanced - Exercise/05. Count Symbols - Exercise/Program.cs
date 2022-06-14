ecf6390d30b8b5c622273496e4818fe6aff2468f
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-14 19:57:23 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Exercise/05. Count Symbols - Exercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] arr = text.ToCharArray();
+            SortedDictionary<char, int> map = new SortedDictionary<char, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(!map.ContainsKey(arr[i]))
+                {
+                    map.Add(arr[i], 1);
+                }
+                else
+                {
+                    map[arr[i]]++;
+                }
+            }
+            foreach (var symbols in map)
+            {
+                Console.WriteLine($"{symbols.Key}: {symbols.Value} time/s");
+            }
+        }
+    }
+}
