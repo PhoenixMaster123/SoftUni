74a39f0906f74015edda557513466e24b72a6a5c
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-14 19:27:23 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Exercise/01. Unique Usernames - Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int input = int.Parse(Console.ReadLine());
+            HashSet<string> usernames = new HashSet<string>();
+            for (int i = 0; i < input; i++)
+            {
+                string persons = Console.ReadLine();
+                usernames.Add(persons);
+
+            }
+            foreach(string name in usernames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
