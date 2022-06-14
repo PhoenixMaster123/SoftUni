c813c787ac9ae7eeba3b304f6e9e689f0342b2ec
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-15 00:09:31 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps in Coding - More Exercises/08. Circle Area and Perimeter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _08._Circle_Area_and_Perimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double perimeter = 2 * Math.PI * r;
+            double area = Math.PI * r * r;
+
+            Console.WriteLine("{0:0.00}", area);
+            Console.WriteLine("{0:0.00}", perimeter);
+        }
+    }
+}
