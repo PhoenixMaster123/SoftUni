2d4156970cbd9a27343aacafbfbddba0d1e37067
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-14 23:45:52 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps in Coding - More Exercises/03. Celsius to Fahrenheit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Celsius_to_Fahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double c = double.Parse(Console.ReadLine());
+            double result = c * 1.8 + 32;
+            Console.WriteLine("{0:0.00}", result);
+        }
+    }
+}
