7e6ca70c6ba490cb89915e47630a60a9a242ddad
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-14 19:29:45 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Exercise/03. Periodic Table - Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Periodic_Table___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            SortedSet<string> chemicalSymbols = new SortedSet<string>();
+            for (int i = 0; i < input; i++)
+            {
+                string[] token = Console.ReadLine().Split(' ');
+                for (int k = 0; k < token.Length; k++)
+                {
+                    chemicalSymbols.Add(token[k]);
+                }
+               
+            }
+            Console.WriteLine(string.Join(" ",chemicalSymbols));
+        }
+    }
+}
