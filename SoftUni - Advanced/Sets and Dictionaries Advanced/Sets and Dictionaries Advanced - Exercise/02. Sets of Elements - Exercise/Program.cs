1cd17212d07e3e8a119992c9e473b37dd7d71de9
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-14 19:28:23 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements - Exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] length = Console.ReadLine().Split(' ');
+            int n = int.Parse(length[0]);
+            int m = int.Parse(length[1]);
+            HashSet<int> firstValues = new HashSet<int>();
+            HashSet<int> secondValues = new HashSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                firstValues.Add(numbers);
+            }
+            for (int k = 0; k < m; k++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+                secondValues.Add(numbers);
+            }
+
+            firstValues.IntersectWith(secondValues);
+            Console.Write(string.Join(" ", firstValues));
+
+
+        }
+    }
+}
