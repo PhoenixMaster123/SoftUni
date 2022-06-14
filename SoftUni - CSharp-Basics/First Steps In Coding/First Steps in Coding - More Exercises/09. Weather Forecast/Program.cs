6de6bc5674ff3d8d67439b9330cd6d11af5dee11
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-15 00:09:55 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/First Steps In Coding/First Steps in Coding - More Exercises/09. Weather Forecast/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _09._Weather_Forecast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string weather = Console.ReadLine();
+
+
+            if (weather == "sunny")
+            {
+
+                Console.WriteLine("It's warm outside!");
+            }
+            else if (weather != "sunny")
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+
+        }
+    }
+}
