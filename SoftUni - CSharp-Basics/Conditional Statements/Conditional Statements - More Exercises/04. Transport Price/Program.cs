ea9f5fbad4779dd1e9b67671f78e75bd1788993f
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-15 23:57:33 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements/Conditional Statements - More Exercises/04. Transport Price/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Transport_Price
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+
+            double taxi = 0.70;
+            double daytarif = 0.79;
+            double nighttarif = 0.90;
+
+            double bus = 0.09;
+            double train = 0.06;
+
+            if (n < 20 && time == "day")
+            {
+                double price = taxi + n * daytarif;
+                Console.WriteLine($"{price:F2}");
+            }
+            else if (n < 20 && time == "night")
+            {
+                double price = taxi + n * nighttarif;
+                Console.WriteLine($"{price:F2}");
+            }
+            else if (n >= 20 && n < 100)
+            {
+                double price = n * bus;
+                Console.WriteLine($"{price:F2}");
+            }
+            else if(n>=100)
+            {
+                double price = n * train;
+                Console.WriteLine($"{price:F2}");
+            }
+
+        }
+    }
+}
