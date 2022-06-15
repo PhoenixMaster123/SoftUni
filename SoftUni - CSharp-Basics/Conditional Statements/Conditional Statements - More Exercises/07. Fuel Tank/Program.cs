ec0ced5dfb1b3103aa55fdf1a6591ce0dacc1ef9
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-15 23:59:33 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements/Conditional Statements - More Exercises/07. Fuel Tank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fuel_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fuel = Console.ReadLine();
+            double liturs = double.Parse(Console.ReadLine());
+
+            if (fuel != "Gas" && fuel != "Diesel" && fuel != "Gasoline")
+            {
+                Console.WriteLine("Invalid fuel!");
+            }
+            
+
+            else if (liturs >= 25 ) //&& fuel == "Gas" && fuel == "Diesel" && fuel == "Gasoline"
+            {
+                 Console.WriteLine($"You have enough {fuel.ToLower()}.");
+                //Console.WriteLine("You have enough {0}", fuel.ToLower());
+            }
+            else if (liturs < 25)
+            {
+               Console.WriteLine($"Fill your tank with {fuel.ToLower()}!"); ;
+            }
+            
+            
+        }
+    }
+}
+
