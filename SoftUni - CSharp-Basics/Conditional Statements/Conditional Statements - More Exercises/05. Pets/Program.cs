87845f1bf5028f4addb1835ba34fc8248c029fd9
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-15 23:58:03 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements/Conditional Statements - More Exercises/05. Pets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int food = int.Parse(Console.ReadLine());
+            double dogfood = double.Parse(Console.ReadLine());
+            double catfood = double.Parse(Console.ReadLine());
+            double turtlefood = double.Parse(Console.ReadLine());
+
+            double alldogfood = days * dogfood;
+            double allcatfood = days * catfood;
+            double allturtlefood = (days * turtlefood) / 1000;
+
+            double allfood = alldogfood + allcatfood + allturtlefood;
+            if (allfood <= food)
+            {
+                double foodremains = food - allfood;
+                Console.WriteLine($"{Math.Floor(foodremains)} kilos of food left.");
+            }
+            else if(allfood>=food)
+            {
+                double foodneeded = allfood - food;
+                Console.WriteLine($"{Math.Ceiling(foodneeded)} more kilos of food are needed.");
+            }
+        }
+    }
+}
