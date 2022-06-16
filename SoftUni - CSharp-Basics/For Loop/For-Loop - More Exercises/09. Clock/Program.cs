3d84830f640d16ef4d5e586e16efa236149618f2
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-16 23:23:46 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For-Loop - More Exercises/09. Clock/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int h = 0; h < 24; h++)
+            {
+                for (int min = 0; min < 60; min++)
+                {
+                    for(int sec=0;sec<60;sec++)
+                    {
+                        Console.WriteLine($"{h:D1} : {min:D1} : {sec:D1}");
+                    }
+                }
+            }
+
+        }
+    }
+}
