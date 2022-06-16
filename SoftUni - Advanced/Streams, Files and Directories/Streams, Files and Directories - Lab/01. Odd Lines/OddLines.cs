0d751727cdbab6c71f511de8aef490440f160cd1
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-16 21:11:44 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Streams, Files and Directories/Streams, Files and Directories - Lab/01. Odd Lines/OddLines.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+
+
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+
+                StreamWriter writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    int lineNum = 0;
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+                        if (lineNum % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        lineNum++;
+                    }
+                }
+            }
+        }
+    }
+}
