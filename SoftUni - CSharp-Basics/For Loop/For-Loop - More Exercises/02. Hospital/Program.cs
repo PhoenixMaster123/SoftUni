17926dc6f0965431aaf7da08d90d263804dbc27e
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-16 23:16:01 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For-Loop - More Exercises/02. Hospital/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+
+            int doctors = 7;
+            int treatment = 0;
+           int  untreatment = 0;
+            for(int i=1;i<=period;i++)
+            {              
+
+                //sum += (patients - doctors);
+                if (period % 3 == 0 && untreatment > treatment) 
+                {
+                    doctors++;
+                    //sum += (patients - doctors);
+                }
+                int patients = int.Parse(Console.ReadLine());
+                if (patients > doctors)
+                {
+                    treatment += doctors;
+                    untreatment += patients - doctors;
+                   
+                }
+                else
+                {
+                    treatment += patients;
+                }              
+            }
+            Console.WriteLine($"Treated patients: {treatment}.");
+            Console.WriteLine($"Untreated patients: {untreatment}.");
+
+        }
+    }
+}
