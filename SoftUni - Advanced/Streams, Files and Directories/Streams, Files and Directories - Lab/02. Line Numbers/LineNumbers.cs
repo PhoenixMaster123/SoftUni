4671f81bb0c274b409aa53ff5f3b10fb82fbce74
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-16 21:16:16 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Streams, Files and Directories/Streams, Files and Directories - Lab/02. Line Numbers/LineNumbers.cs
@@ -0,0 +1,35 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\input.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+
+                StreamWriter writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    int lineNum = 1;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                      
+                        writer.WriteLine(lineNum + ". " + line);
+                        lineNum++;
+                    }
+                }
+            }
+        }
+    }
+}
