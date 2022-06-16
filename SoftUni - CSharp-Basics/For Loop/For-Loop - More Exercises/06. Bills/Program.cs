909a4e78f1c137099fb567c9f56580f5e3921d01
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-16 23:20:40 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For-Loop - More Exercises/06. Bills/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Bills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = int.Parse(Console.ReadLine());
+            double sum = 0;
+            double water = 0;
+            double internet = 0;
+            double other = 0;
+            double average = 0;
+            double result = 0;
+            double result2 = 0;
+            for (int i = 1; i <= month; i++)
+            {
+                double electrisity = double.Parse(Console.ReadLine());
+                
+                if(electrisity!=0)
+                {
+                    sum += electrisity;
+                    water = month * 20;
+                    internet = month * 15;
+                    other = (electrisity + 20 + 15) * 1.2;
+                }
+                result += other;
+            }
+            result2 += result;
+            average = (sum + water + internet + result2) / month;
+            Console.WriteLine($"Electricity: {sum:F2} lv");
+            Console.WriteLine($"Water: {water:F2} lv");
+            Console.WriteLine($"Internet: {internet:F2} lv");
+            Console.WriteLine($"Other: {result2:F2} lv");
+            Console.WriteLine($"Average: {average:F2} lv");
+
+
+
+
+
+        }
+    }
+}
