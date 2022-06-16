1c768198b8aed06b3259e13f7b36dbb93e9f34e3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-16 23:22:12 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/For-Loop - More Exercises/07. Football League/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Football_League
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capasity = int.Parse(Console.ReadLine());
+            double fans = int.Parse(Console.ReadLine());           
+            double fansA = 0;
+            double fansB = 0;
+            double fansV = 0;
+            double fansG = 0;
+            double allFans = 0;
+          
+            for(int i=1;i<=fans;i++)
+            {
+                string team = Console.ReadLine();
+                if (team == "A")
+                {
+                    fansA++;
+                }
+                else if (team == "B") 
+                {
+                    fansB++;
+                }
+                else if (team == "V")
+                {
+                    fansV++;
+                }
+                else
+                {
+                    fansG++;
+                }
+
+            }
+            fansA = fansA / fans * 100;
+            fansB = fansB / fans * 100;
+            fansV = fansV / fans * 100;
+            fansG = fansG / fans * 100;
+            allFans = fans / capasity*100;
+            Console.WriteLine($"{fansA:F2}%");
+            Console.WriteLine($"{fansB:F2}%");
+            Console.WriteLine($"{fansV:F2}%");
+            Console.WriteLine($"{fansG:F2}%");
+            Console.WriteLine($"{allFans:F2}%");
+
+        }
+    }
+}
