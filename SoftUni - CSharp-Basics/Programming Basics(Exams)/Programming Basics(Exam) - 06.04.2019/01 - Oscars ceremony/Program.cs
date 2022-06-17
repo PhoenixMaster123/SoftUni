3efdca46138bc14d3fca2813f3a1ff312158f6a8
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-17 22:35:53 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics(Exam) - 06.04.2019/01 - Oscars ceremony/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T1___Oscars_ceremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+            double statuet = 0.3;
+            double keturing = 0.15;
+
+            double resultstat = rent - rent * statuet;
+            double resultketuring = resultstat - resultstat * keturing;
+            double sound = resultketuring / 2;
+
+            Console.WriteLine($"{rent + resultstat + resultketuring + sound:F2}");
+        }
+    }
+}
