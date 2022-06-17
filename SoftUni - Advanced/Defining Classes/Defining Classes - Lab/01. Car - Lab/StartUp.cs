442762ed4fd302efc6189930149423aa62d36e13
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-17 22:48:42 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Defining Classes/Defining Classes - Lab/01. Car - Lab/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{  public class StartUp
+
+{
+    static void Main()
+    {
+        Car car = new Car();
+
+        car.Make = "Mercedes";
+        car.Model = "S-Class";
+        car.Year = 2003;
+
+
+       Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+    }
+
+}
+
+}
+
