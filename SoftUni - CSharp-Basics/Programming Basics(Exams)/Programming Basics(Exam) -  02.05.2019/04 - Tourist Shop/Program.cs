c35fd64eee0085505a1b22ff44e3759fa10807be
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-17 22:44:55 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics(Exam) -  02.05.2019/04 - Tourist Shop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace T5___Tourist_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string equipment = "";
+            int count = 0;
+            double money = 0;
+            while(true)
+            {
+                equipment = Console.ReadLine();
+                if(equipment=="Stop")
+                {
+                    break;
+                }
+                double price = double.Parse(Console.ReadLine());             
+                count++;
+                if (count % 3 == 0)
+                {
+                    price /= 2;
+                }
+                money += price;
+                budget -= price;
+                if(budget<0)
+                {
+                    break;
+                }
+                
+
+            }
+
+            if (budget >= 0)
+            {
+                Console.WriteLine($"You bought {count} products for {money:f2} leva.");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough money!");
+                Console.WriteLine($"You need {Math.Abs(budget):f2} leva!");
+            }
+        }
+    }
+}
