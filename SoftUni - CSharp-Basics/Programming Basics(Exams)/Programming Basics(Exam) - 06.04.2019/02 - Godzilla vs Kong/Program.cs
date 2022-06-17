f0822adb8d4355337d6c4d2c7c57b265ba125556
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-17 22:36:40 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics(Exam) - 06.04.2019/02 - Godzilla vs Kong/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace T2___Godzilla_vs_Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statists = int.Parse(Console.ReadLine());
+            double cloets = double.Parse(Console.ReadLine());
+            double percent = 0;
+          
+            double decor = budget * 0.1;
+            double sumcloets = cloets * statists;
+            if (statists > 150)
+            {
+                 percent += sumcloets * 0.1;
+            }
+            sumcloets -= percent;
+           
+            double allsum = decor + sumcloets;
+            
+            if(budget>=allsum)
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget-allsum:F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {allsum - budget:F2} leva more.");
+            }
+
+        }
+    }
+}
