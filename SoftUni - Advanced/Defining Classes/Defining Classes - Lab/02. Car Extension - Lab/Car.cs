0772a521fea00a1be44c3991831176260864000b
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-17 22:49:36 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Defining Classes/Defining Classes - Lab/02. Car Extension - Lab/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double fuelToConsume = distance * FuelConsumption;
+            if (FuelQuantity - fuelToConsume >= 0)
+            {
+                FuelQuantity -= fuelToConsume;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            /////////////////////////////////////////// First Variant /////////////////////////////
+            
+            return $"Make: {Make}\nModel: {Model}\nYear: {Year}\nFuel: {FuelQuantity:F2}";
+
+            /////////////////////////////////////////// Second Variant /////////////////////////////
+            
+            //StringBuilder result = new StringBuilder();
+
+            //result.AppendLine($"Make: {this.Make}");
+            //result.AppendLine($"Model: {this.Model}");
+            //result.AppendLine($"Year: {this.Year}");
+            //result.AppendLine($"Fuel: {this.FuelQuantity:F2}L");
+
+            //return result.ToString().TrimEnd();
+
+        }
+
+    }
+}
