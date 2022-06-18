882bd97f10b0357ce35672f6daa6da0379dcfffa
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-19 00:09:35 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Lab/07. Working Hours/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hoursday = int.Parse(Console.ReadLine());
+            string days = Console.ReadLine();
+            if (hoursday >= 10 && hoursday < 18 || days == "Monday" && days == "Tuesday" && days == "Wednesday" && days == "Thursday" && days == "Friday" && days == "Saturday") 
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+            
+        }
+    }
+}
