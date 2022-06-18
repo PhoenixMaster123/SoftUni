acd0f1c379a61b96ea6dd54a7f6fba1452db67c1
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-19 00:10:25 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Lab/08. Cinema Ticket/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cinema_Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string days = Console.ReadLine();
+            switch (days)
+            {
+                case "Monday":
+                    Console.WriteLine(12);
+                    break;
+                case "Tuesday":
+                    Console.WriteLine(12);
+                    break;
+                case "Wednesday":
+                    Console.WriteLine(14);
+                    break;
+                case "Thursday":
+                    Console.WriteLine(14);
+                    break;
+                case "Friday":
+                    Console.WriteLine(12);
+                    break;
+                case "Saturday":
+                    Console.WriteLine(16);
+                    break;
+                case "Sunday":
+                    Console.WriteLine(16);
+                    break;
+
+            }
+
+        }
+
+    }
+}
