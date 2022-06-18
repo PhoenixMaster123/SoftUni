f53e93bbbb3a31d1c46e55076c4837ca794e9cc3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-18 23:27:49 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Lab/04. Personal Titles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Personal_Titles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (gender == "m" && age < 16) 
+            {
+                Console.WriteLine("Master");
+            }
+            else if(gender == "m" && age >= 16)
+            {
+                Console.WriteLine("Mr.");
+            }
+            if (gender == "f" && age < 16)
+            {
+                Console.WriteLine("Miss");
+            }
+            else if (gender == "f" && age >= 16)
+            {
+                Console.WriteLine("Ms.");
+            }
+        }
+    }
+}
