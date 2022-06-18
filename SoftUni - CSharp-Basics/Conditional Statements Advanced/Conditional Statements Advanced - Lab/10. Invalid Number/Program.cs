2899f84af16e4e7ff9cd1da3a7e061bc5dba5544
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-19 00:45:08 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Lab/10. Invalid Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Invalid_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num >= 100 && num <= 200 || num == 0)
+            {
+                Console.WriteLine("");
+            }
+            else if(num<100||num>200)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
