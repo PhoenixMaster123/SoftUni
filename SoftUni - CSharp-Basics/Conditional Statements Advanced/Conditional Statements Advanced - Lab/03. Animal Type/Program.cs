a1fbe90d35cb0d3d715a8c62e955e7737f3b5e52
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-18 23:27:07 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Lab/03. Animal Type/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Animal_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            switch(animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                case "crocodile":
+                    Console.WriteLine("reptile");
+                    break;
+                case "tortoise":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+
+            }
+        }
+    }
+}
