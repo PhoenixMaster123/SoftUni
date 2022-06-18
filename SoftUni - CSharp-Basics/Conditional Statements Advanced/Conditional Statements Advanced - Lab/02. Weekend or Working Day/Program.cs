ad9fc66ebc3804b0bf9cd2ee084d21ae5f48817d
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-18 23:32:14 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Lab/02. Weekend or Working Day/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Weekend_or_Working_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string days = Console.ReadLine();
+            switch(days)
+            {
+                case "Monday":
+                    Console.WriteLine("Working day");
+                    break;
+                case "Tuesday":
+                    Console.WriteLine("Working day");
+                    break;
+                case "Wednesday":
+                    Console.WriteLine("Working day");
+                    break;
+                case "Thursday":
+                    Console.WriteLine("Working day");
+                    break;
+                case "Friday":
+                    Console.WriteLine("Working day");
+                    break;              
+                case "Saturday":
+                    Console.WriteLine("Weekend");
+                    break;
+                case "Sunday":
+                    Console.WriteLine("Weekend");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+
+            }
+        }
+    }
+}
