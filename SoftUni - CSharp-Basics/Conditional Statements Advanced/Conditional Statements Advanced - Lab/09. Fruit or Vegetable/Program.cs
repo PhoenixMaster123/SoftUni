29f7517fa0109119e45e9e3e7409740acaa0be0c
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-19 00:42:43 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Lab/09. Fruit or Vegetable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fruit_or_Vegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string something = Console.ReadLine();
+            if (something == "banana" || something == "apple" || something == "kiwi" || something == "cherry" || something == "lemon" || something == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (something == "tomato" || something == "cucumber" || something == "pepper" || something == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
