e40e03a6d6d5e3ec6be8a1f80f7e497f241c38a6
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-19 19:29:11 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Exercise/02. Summer Outfit/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Summer_Outfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grad = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
+
+            if (grad >= 10 && grad <= 18 && time == "Morning")
+            {
+                outfit = "Sweatshirt";
+                shoes = "Sneakers";
+            }
+            else if (grad >= 10 && grad <= 18 && time == "Afternoon")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+            else if (grad >= 10 && grad <= 18 && time == "Evening")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+            if (grad > 18 && grad <= 24 && time == "Morning")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+            else if(grad > 18 && grad <= 24 && time == "Afternoon")
+            {
+                outfit = "T-Shirt";
+                shoes = "Sandals";
+            }
+            else if (grad > 18 && grad <= 24 && time == "Evening")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+            if (grad >= 25 && time == "Morning")
+            {
+                outfit = "T-Shirt";
+                shoes = "Sandals";
+            }
+            else if (grad >= 25 && time == "Afternoon") 
+            {
+                outfit = "Swim Suit";
+                shoes = "Barefoot";
+            }
+            else if(grad >= 25 && time == "Evening")
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+            Console.WriteLine($"It's {grad} degrees, get your {outfit} and {shoes}.");
+
+        }
+    }
+}
