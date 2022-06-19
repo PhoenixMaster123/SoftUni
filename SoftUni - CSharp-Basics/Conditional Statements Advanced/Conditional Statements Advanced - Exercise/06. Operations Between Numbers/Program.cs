e7d66a03bdaecf5f20dc1189210ff3dc8aef30d5
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-19 19:35:56 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Exercise/06. Operations Between Numbers/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Operations_Between_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = int.Parse(Console.ReadLine());
+            double n2 = int.Parse(Console.ReadLine());
+            string operators = Console.ReadLine();
+            double result = 0.0;
+            if(operators=="+")
+            {
+                result = n1 + n2;
+                if (result % 2 == 0) 
+                {
+                    Console.WriteLine($"{n1} + {n2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} + {n2} = {result} - odd");
+                }
+            }
+            else if (operators == "-")
+            {
+                result = n1 - n2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} - {n2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} - {n2} = {result} - odd");
+                }
+            }
+            else if (operators == "*")
+            {
+                result = n1 * n2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{n1} * {n2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{n1} * {n2} = {result} - odd");
+                }
+            }
+            else if(operators == "/")
+            {
+                result = n1 / n2;
+                if (n2!=0)
+                {
+                    Console.WriteLine($"{n1} / {n2} = {result:F2}");
+                }
+                else if (n2==0) 
+                {
+                    Console.WriteLine($"Cannot divide {n1} by zero");
+                }
+            }
+            else if (operators == "%")
+            {
+                result = n1 % n2;
+
+                if (n2!=0)
+                {
+                    Console.WriteLine($"{n1} % {n2} = {result}");
+                }
+                else if(n2==0)
+                {
+                    Console.WriteLine($"Cannot divide {n1} by zero");
+                }
+            }
+        }
+    }
+}
+
+
+            /*switch (operators)
+            {
+                case "+":
+                    int result = n1 + n2;
+                    int even = result % 2;
+                    if (even==result)
+                    {
+
+                        Console.WriteLine($"{result} even");
+                    }
+                    break;*/
+        
+        
+    
