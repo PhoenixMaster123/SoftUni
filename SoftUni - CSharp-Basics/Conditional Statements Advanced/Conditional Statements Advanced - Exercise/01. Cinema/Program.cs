3df121ad814206ee8d13d5a204f1072228849cba
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-19 19:28:17 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Exercise/01. Cinema/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projections = Console.ReadLine();
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            double premiere = 12.00;
+            double normal = 7.50;
+            double discount = 5.00;
+            double price = 0;
+
+            if (projections == "Premiere")
+            {
+                price = premiere * r * c;
+            }
+            else if (projections == "Normal")
+            {
+                price = normal * r * c;
+            }
+            else if (projections == "Discount") 
+            {
+                price = discount * r * c;
+            }
+            Console.WriteLine($"{price:F2} leva");
+
+        }
+    }
+}
