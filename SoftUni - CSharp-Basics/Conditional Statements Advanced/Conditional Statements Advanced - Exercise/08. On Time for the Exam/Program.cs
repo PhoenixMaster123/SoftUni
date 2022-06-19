d921c801bd16efc9abf8868618141b469b40714d
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-19 19:39:36 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - Exercise/08. On Time for the Exam/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace On_Time_for_the_Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hoursexam = int.Parse(Console.ReadLine());
+            int minutsexam = int.Parse(Console.ReadLine());
+            int cominghours = int.Parse(Console.ReadLine());
+            int comingminuts = int.Parse(Console.ReadLine());
+
+            int difference = 0;
+            int hour = 0;
+            int minuts = 0;
+
+            minutsexam += hoursexam * 60;
+            comingminuts += cominghours * 60;
+
+            if(comingminuts>minutsexam)
+            {
+                Console.WriteLine("Late");
+                difference = comingminuts - minutsexam;
+                if(difference<60)
+                {
+                    Console.WriteLine($"{(difference)} minutes after the start");
+                }
+                else
+                {
+                    hour = difference / 60;
+                    minuts = difference % 60;
+                    Console.WriteLine($"{hour}:{minuts:d2} hours after the start");
+                }
+            }
+            else if (comingminuts < minutsexam - 30)
+            {
+                Console.WriteLine("Early");
+                difference = minutsexam - comingminuts;
+                if (difference < 60)
+                {
+                    Console.WriteLine($"{Math.Abs(difference)} minutes before the start");
+                }
+                else
+                {
+                    hour = difference / 60;
+                    minuts = difference % 60;
+                    Console.WriteLine($"{hour}:{minuts:d2} hours before the start");
+                }
+            }
+            else
+            {
+                Console.WriteLine("On time");
+                difference = minutsexam - comingminuts;
+                Console.WriteLine($"{difference} minutes before the start");
+            }
+
+
+           /* if (hoursexam == minutsexam && cominghours == comingminuts ||minutsexam <=30 && cominghours==comingminuts)
+            {
+                Console.WriteLine("On time");
+            }*/
+
+
+        }
+    }
+}
