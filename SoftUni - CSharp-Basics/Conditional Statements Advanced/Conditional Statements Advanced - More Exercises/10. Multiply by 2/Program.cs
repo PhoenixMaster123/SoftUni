acd20ecf8f4093121895550c4d1a1bafe5184f8d
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-20 21:22:36 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - More Exercises/10. Multiply by 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10._Multiply_by_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+                double number = double.Parse(Console.ReadLine());
+                while (true)
+                {
+                    if (number < 0)
+                    {
+                        Console.WriteLine("Negative number!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Result: {number * 2:F2}");
+                    }
+                    number = double.Parse(Console.ReadLine());
+                }
+        }
+    }
+}
