655a0eef1dcc3708d57faf781776d53b23e2d3c2
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-20 21:04:20 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - More Exercises/04. Car To Go/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Car_To_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string clas = "";
+            string car = "";
+
+            if (budget <= 100)
+            {
+                clas = "Economy class";
+                if (season == "Summer")
+                {
+                    car = "Cabrio";
+                    budget = budget * 0.35;
+                }
+                else if (season == "Winter")
+                {
+                    car = "Jeep";
+                    budget = budget * 0.65;
+                }
+
+            }
+            else if (budget > 100 && budget <= 500)
+            {
+                clas = "Compact class";
+                if (season == "Summer")
+                {
+                    car = "Cabrio";
+                    budget = budget * 0.45;
+                }
+                else if (season == "Winter")
+                {
+                    car = "Jeep";
+                    budget = budget * 0.80;
+                }
+            }
+            else if (budget > 500) 
+            {
+                clas = "Luxury class";
+                if (season=="Summer"||season=="Winter")
+                {
+                    car = "Jeep";
+                    budget = budget * 0.90;
+                }
+            }
+            Console.WriteLine($"{clas}");
+            Console.WriteLine($"{car} - {budget:F2}");
+        }
+    }
+}
