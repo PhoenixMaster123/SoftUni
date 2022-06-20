884c561167660ec32b9efd714f2a3c1a3ff594c5
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-20 21:07:45 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - More Exercises/07. School Camp/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Camp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            string group = Console.ReadLine();
+            int students = int.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+            double price = 0;
+            string sport = "";
+
+            if (group == "boys" && season == "Winter" || group == "girls" && season == "Winter")
+            {
+                price = students * nights * 9.60;
+                if (group == "girls")
+                {
+                    sport = "Gymnastics";
+                }
+                else
+                {
+                    sport = "Judo";
+                }
+            }
+            else if (group == "mixed" && season == "Winter")
+            {
+                price = students * nights * 10;
+                if (group == "mixed")
+                {
+                    sport = "Ski";
+                }
+
+            }
+
+            else if (group == "boys" && season == "Spring" || group == "girls" && season == "Spring")
+            {
+                price = students * nights * 7.20;
+                if (group == "girls")
+                {
+                    sport = "Athletics";
+                }
+                else
+                {
+                    sport = "Tennis";
+                }
+            }
+            else if (group == "mixed" && season == "Spring")
+            {
+                price = students * nights * 9.50;
+                if (group == "mixed")
+                {
+                    sport = "Cycling";
+                }
+            }
+
+            else if (group == "boys" && season == "Summer" || group == "girls" && season == "Summer") 
+            {
+                price = students * nights * 15;
+                if (group == "girls")
+                {
+                    sport = "Volleyball";
+                }
+                else
+                {
+                    sport = "Football";
+                }
+            }
+            else if (group == "mixed" && season == "Summer")
+            {
+                price = students * nights * 20;
+                if (group == "mixed")
+                {
+                    sport = "Swimming";
+                }
+            }
+            if (students >= 50)
+            {
+                price = price - price * 0.50;
+            }
+            else if (students >= 20 && students < 50)
+            {
+                price = price - price * 0.15;
+            }
+            else if (students >= 10 && students < 20)
+            {
+                price = price - price * 0.05;
+            }
+            Console.WriteLine($"{sport} {price:F2} lv.");
+
+        }
+    }
+}
