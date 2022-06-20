59eceb3ae21346a9bd92fd897b95e67ffcd2db0a
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-20 21:09:10 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - More Exercises/09. Numbers from 1 to 10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_from_1_to_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while(num<=10)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+        }
+    }
+}
