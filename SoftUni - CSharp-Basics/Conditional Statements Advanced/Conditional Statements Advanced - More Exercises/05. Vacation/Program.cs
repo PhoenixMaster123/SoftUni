120de2556f2ea5c4c35edbd1ad6a57f7615edf32
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-20 21:05:10 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/Conditional Statements Advanced/Conditional Statements Advanced - More Exercises/05. Vacation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string location = "";
+            string place = "";
+
+            if (budget <= 1000)
+            {
+                place = "Camp";
+                if (season == "Summer")
+                {
+                    location = "Alaska";
+                    budget = budget * 0.65;
+                }
+                else if (season == "Winter")
+                {
+                    location = "Morocco";
+                    budget = budget * 0.45;
+                }
+            }
+            else if (budget > 1000 && budget <= 3000)
+            {
+                place = "Hut";
+                if (season == "Summer")
+                {
+                    location = "Alaska";
+                    budget = budget * 0.80;
+                }
+                else if (season == "Winter")
+                {
+                    location = "Morocco";
+                    budget = budget * 0.60;
+                }
+            }
+            else if (budget > 3000) 
+            {
+                place = "Hotel";
+                if (season == "Summer")
+                {
+                    location = "Alaska";
+                    budget = budget * 0.90;
+                }
+                else if (season == "Winter")
+                {
+                    location = "Morocco";
+                    budget = budget * 0.90;
+                }
+            }
+            Console.WriteLine($"{location} - {place} - {budget:F2}");
+        }
+    }
+}
