118a885b6f5e2ad4ec2a75a5901456972873b208
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-21 18:58:10 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Exercise/01. Action Print - Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Action_Print___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();         
+            Action<string[]> print = name => Console.WriteLine(string.Join(Environment.NewLine, name));
+            print(names);
+
+           
+        }
+    }
+}
