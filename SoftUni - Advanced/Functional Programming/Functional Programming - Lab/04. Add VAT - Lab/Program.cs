eab448940e1b319e76f47a042853ee849c5d707c
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-21 18:44:22 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Lab/04. Add VAT - Lab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////////////////////////////// First Variant /////////////////////////////
+
+
+            //double[] numbers = Console.ReadLine().Split(", ").Select(double.Parse).Select(x => x * 1.2).ToArray();
+            //foreach (double price in numbers)
+            //{
+            //    Console.WriteLine($"{price:F2}");
+            //}
+            /////////////////////////////////////////////////////////////////// Second Variant /////////////////////////////
+            Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).Select(p => p * 1.2m).ToList().ForEach(p => Console.WriteLine($"{p:F2}"));
+
+        }
+    }
+}
