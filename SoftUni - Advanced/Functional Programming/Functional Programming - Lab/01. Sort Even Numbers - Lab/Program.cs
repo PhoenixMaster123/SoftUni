0afa89089fb59f3c47a7a24a6b9ef4beb42029a9
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-21 18:42:50 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Lab/01. Sort Even Numbers - Lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Sort_Even_Numbers___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+            Console.WriteLine(string.Join(", ",numbers));
+        }
+    }
+}
