660feb461562f2f37a04b0c3918ae0b044750ed4
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-21 18:44:43 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Lab/05. Filter By Age - Lab/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Filter_By_Age___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            var people = new Dictionary<string, int>();
+
+            for (int i = 0; i < input; i++)
+            {
+                string[] token = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                if (!people.ContainsKey(token[0]))
+                {
+                    people[token[0]] = int.Parse(token[1]);
+                }
+            }
+
+            var condition = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var format = Console.ReadLine();
+
+            Func<int, bool> filter;
+            if (condition == "younger")
+            {
+                filter = x => x < age;
+            }
+            else
+            {
+                filter = x => x >= age;
+            }
+
+            if (format == "age")
+            {
+                Print(people, filter, (key, value) => value.ToString());
+            }
+            else if (format == "name")
+            {
+                Print(people, filter, (key, value) => key);
+            }
+            else
+            {
+                Print(people, filter, (key, value) => key + " - " + value);
+            }
+        }
+        static void Print(Dictionary<string, int> people, Func<int, bool> filter, Func<string, int, string> print)
+        {
+            foreach (var kvp in people)
+            {
+                var name = kvp.Key;
+                var age = kvp.Value;
+
+                if (filter(age))
+                {
+                    Console.WriteLine(print(name, age));
+                }
+            }
+        }
+    }
+}
