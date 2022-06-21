e68777b8a0f7a21d0090b5dbe0a4b130fdfcb036
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-21 18:43:34 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Lab/02. Sum Numbers - Lab/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int result = numbers.Sum();
+            int count = numbers.Length;
+            Console.WriteLine(count);
+            Console.WriteLine(result);
+        }
+    }
+}
