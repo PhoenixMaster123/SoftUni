0f20ed0ae4326268ab7fa48f9da40d66915d0e32
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-22 00:20:19 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Exercise/04. Find Evens or Odds - Exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Predicate<int> even = x => x % 2 == 0;
+            Action<List<int>> print = x => Console.WriteLine(string.Join(" ", x));
+
+            int[] range = Console.ReadLine()
+                 .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            string condition = Console.ReadLine();
+
+            int start = range[0];
+            int end = range[1];
+
+            List<int> numbers = new List<int>();
+
+            for (int k = start; k <= end; k++)
+            {
+                numbers.Add(k);
+            }
+
+            if (condition == "odd")
+            {
+                numbers.RemoveAll(even);
+            }
+            else if (condition == "even")
+            {
+                numbers.RemoveAll(x => !even(x));
+            }
+
+            print(numbers);
+        }
+    }
+}
