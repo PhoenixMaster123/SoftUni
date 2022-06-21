fc6e85248487d4e9319ea21bfca976697a72b705
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-21 18:44:00 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Lab/03. Count Uppercase Words - Lab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(x => char.IsUpper(x[0])).ToList().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
