593cdcfd9c81f51bb8309fed87b5879c74165949
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-22 00:13:52 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Exercise/03. Custom Min Function - Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Custom_Min_Function___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////////////////////////////////////////////// First Variant //////////////////////////////////////////
+
+            //List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //Console.WriteLine(input.Min());
+
+            ////////////////////////////////////////////////// Second Variant //////////////////////////////////////////
+            //Func<int[], int> minNumber = n => n.Min();
+            //int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            //Console.WriteLine(minNumber(numbers));
+
+            ////////////////////////////////////////////////// Third Variant //////////////////////////////////////////
+            int number = Console.ReadLine().Split().Select(int.Parse).Min();
+            Console.WriteLine(number);
+        }
+    }
+}
