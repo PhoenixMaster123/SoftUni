7dfb40c1aa1622a510e95cfc9bbf88bc23f3503e
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-22 00:13:25 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Exercise/02. Knights of Honor - Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Knights_of_Honor___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /////////////////////////////////////////////////////////////// First Variant //////////////////////////////////
+
+            //string[] names = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).ToArray();       
+            //Action<string> appendSir = name => Console.WriteLine($"Sir {name}");      
+            //foreach (var name in names)
+            //{
+            //    appendSir(name);
+            //}
+            /////////////////////////////////////////////////////////////// Second Variant //////////////////////////////////
+            
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList().ForEach(name => Console.WriteLine("Sir " + name));
+        }
+    }
+}
