c4bcde39ca80c2c041511fc9dd6239684d514582
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-22 22:26:16 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Defining Classes/Defining Classes - Lab/05. Special Cars/Tire.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5_Special_Cars
+{
+    public class Tire
+    {
+        public int Year { get; set; }
+
+        public double Pressure { get; set; }
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+    }
+
+    public class Engine
+    {
+        public int HorsePower { get; set; }
+
+        public double CubicCapacity { get; set; }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            this.HorsePower = horsePower;
+            this.CubicCapacity = cubicCapacity;
+        }
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine,
+            Tire[] tires)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - ((this.FuelConsumption * distance) / 100) >= 0)
+            {
+                this.FuelQuantity -= ((distance * this.FuelConsumption) / 100);
+            }
+        }
+    }
+
+}
