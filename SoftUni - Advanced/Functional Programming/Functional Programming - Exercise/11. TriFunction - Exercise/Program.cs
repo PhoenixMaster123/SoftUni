32caa51bee23d80c31660457652fd1ec211d8ee1
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-22 22:09:18 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Functional Programming/Functional Programming - Exercise/11. TriFunction - Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _11._TriFunction___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(' ');
+
+            Func<string, int, bool> validator = (name, value) => name.ToCharArray().Select(currChar => (int)currChar).Sum() >= numbers;
+            Func<string[], int, Func<string, int, bool>, string> foundName = (collection, value, func) => collection.FirstOrDefault(name => func(name, value));
+
+            Console.WriteLine(foundName(names, numbers, validator));
+        }
+    }
+}
