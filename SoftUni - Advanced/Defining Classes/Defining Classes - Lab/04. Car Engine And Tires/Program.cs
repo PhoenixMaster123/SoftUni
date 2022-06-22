7504a663b85e707f434d0b827d4da91c22e3711b
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-22 22:24:45 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Defining Classes/Defining Classes - Lab/04. Car Engine And Tires/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine v12 = new Engine(580, 1200);
+            Tire[] tires = new Tire[]
+            {
+                new Tire(2018, 2.3),
+                new Tire(2018, 2.5),
+                new Tire(2018, 2.2),
+                new Tire(2018, 2.3)
+            };
+            Car bmv = new Car("BMW", "X6", 1993, 5003, -50, v12, tires);
+
+            Console.WriteLine("Horse power: " + bmv.Engine.HorsePower);
+
+            foreach (var tire in bmv.Tires)
+            {
+                Console.WriteLine($"{tire.Year} - {tire.Pressure}");
+            }
+
+
+            
+            //Car defaultGolf = new Car();
+
+            //Console.WriteLine($"Default Golf: " + defaultGolf.WhoAmI());
+            //Car car = new Car();
+
+            //car.Make = "VW";
+            //car.Model = "MK3";
+            //car.Year = 1992;
+            //car.FuelQuantity = 200;
+            //car.FuelConsumption = 200;
+            //car.Drive(2000);
+
+            //Console.WriteLine(car.WhoAmI());
+
+        }
+
+    }
+}
