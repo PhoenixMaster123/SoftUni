2060f26f0182ddae05b21d0fcc8ad753f52fdd19
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-23 22:46:29 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Generics/Generics - Exercise/03. Generic Swap Method String/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P03.GenericSwapMethodString
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            Box<string> box = new Box<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string value = Console.ReadLine();
+                box.Items.Add(value);
+            }
+
+            string indeces = Console.ReadLine();
+            int index1 = int.Parse(indeces.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0]);
+            int index2 = int.Parse(indeces.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]);
+
+            box.Swap(index1, index2);
+
+            Console.WriteLine(box);
+        }
+    }
+}
