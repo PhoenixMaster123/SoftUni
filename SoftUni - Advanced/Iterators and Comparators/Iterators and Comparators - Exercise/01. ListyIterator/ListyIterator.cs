fc78afcf883ab235d4247f3b60670153963da2ac
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-24 20:08:54 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Iterators and Comparators/Iterators and Comparators - Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> collection;
+
+        private int currentIndex;
+
+        public ListyIterator(params T[] data)
+        {
+            collection = new List<T>(data);
+            currentIndex = 0;
+        }
+
+        public List<T> List => collection;
+
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                currentIndex++;
+            }
+
+            return canMove;
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine($"{collection[currentIndex]}");
+        }
+
+        public bool HasNext() => currentIndex < collection.Count - 1;
+    }
+}
