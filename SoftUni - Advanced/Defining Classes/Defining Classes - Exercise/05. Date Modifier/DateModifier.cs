9af92e936d1d05414f4671b7c7ac9e14cf2a7fae
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-24 19:40:51 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Defining Classes/Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5.Date_Modifier
+{
+    public static class DateModifier
+    {
+        public static int GetDiffBetweendatesInDays(string dateOneStr, string dateTwoStr)
+        {
+            //DateTime dateOne = new DateTime();
+            DateTime dateOne = DateTime.Parse(dateOneStr);
+            DateTime dateTwo = DateTime.Parse(dateTwoStr);
+
+            TimeSpan diff = dateOne - dateTwo;
+            return Math.Abs(diff.Days);
+        }
+    }
+}
---
+++ SoftUni - Advanced/Defining Classes/Defining Classes - Exercise/05. Date Modifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5.Date_Modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int days = DateModifier.GetDiffBetweendatesInDays(firstDate,secondDate);
+            Console.WriteLine(days);
+        }
+    }
+}
