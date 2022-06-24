4c37b8e623e76cea4280a5fa112af41cc31795d7
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-24 19:39:40 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Defining Classes/Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            this.Members = new List<Person>() ;
+        }
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person person)
+        {
+            this.Members.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person person = this.Members
+                .OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+
+            return person;                
+        }
+    }
+}
