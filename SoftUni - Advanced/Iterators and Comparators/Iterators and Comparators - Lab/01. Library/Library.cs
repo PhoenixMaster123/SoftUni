ac5ddf41470b3918649a88d7fa95557e5f8e277a
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-24 19:56:33 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Iterators and Comparators/Iterators and Comparators - Lab/01. Library/Library.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IteratorsAndComparators;
+
+namespace IteratorsAndComparators
+{
+    public class Library: IEnumerable< Book>
+    {
+        public Library(params Book [] books)
+        {
+            Books = books.ToList();
+        }
+
+        public List< Book > Books { get; set; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(Books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator: IEnumerator<Book>
+        {
+            public bool MoveNext()
+            {
+                return ++index<Books .Count ;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+
+            private int index= -1;
+
+            public LibraryIterator(List< Book>books)
+            {
+                Books = books;
+            }
+            
+            public List< Book> Books { get; set; }
+
+            public Book Current => Books[index];
+
+            object IEnumerator.Current => Books[index];
+
+            public void Dispose() {}
+        }
+    }
+}
