8bc7c71afbfba21f0a4c894df0d291336aa5cd4b
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-25 19:57:24 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Iterators and Comparators/Iterators and Comparators - Exercise/03. Stack/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+
+            while (true)
+            {
+                var tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens[0] == "END")
+                {
+                    break;
+                }
+                else if (tokens[0] == "Push")
+                {
+                    stack.Push(tokens.Skip(1).Select(t => t.Split(',').First()).ToArray());
+                }
+                else if (tokens[0] == "Pop")
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (ArgumentException ae)
+                    {
+                        Console.WriteLine(ae.Message);
+                    }
+                }
+            }
+
+            foreach (string element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            foreach (string element in stack)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
