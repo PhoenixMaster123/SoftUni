f47bf37ee06801d8f99820aad6ac992c94c99fc0
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-25 19:58:14 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Iterators and Comparators/Iterators and Comparators - Exercise/04. Froggy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lake = new Lake(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Console.WriteLine(string .Join( ", ",lake ));
+        }
+    }
+}
