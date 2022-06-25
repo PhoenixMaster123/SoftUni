283905e22fb76566abf5ba6f8efb4f2337a911bd
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-25 20:04:15 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Streams, Files and Directories/Streams, Files and Directories - Exercise/02. Line Numbers part 2/LineNumbers.cs
@@ -0,0 +1,42 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\text.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            ProcessLines(inputPath, outputPath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+
+            using (writer)
+            {
+                using (reader)
+                {
+                    int counter = 1;
+                    int countLetters = 0;
+                    int countSymbols = 0;
+
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        countLetters = line.Count(char.IsLetter);
+                        countSymbols = line.Count(char.IsPunctuation);
+                        writer.WriteLine($"Line {counter}: {line} ({countLetters})({countSymbols})");
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
