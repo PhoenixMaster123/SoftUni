9b5d924f3548d06880abc68d90fab024355e5091
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-25 19:59:48 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Iterators and Comparators/Iterators and Comparators - Exercise/6.Equality_Logic/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6.Equality_Logic
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name => name;
+
+        public int Age => age;
+
+        public int CompareTo(Person other)
+        {
+            int result = Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result = Age.CompareTo(other.Age);
+            }
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Age.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Person;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Age == other.Age && Name == other.Name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+}
