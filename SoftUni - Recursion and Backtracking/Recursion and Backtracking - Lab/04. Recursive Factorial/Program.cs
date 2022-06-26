f97aeb7a8dc8bfd05c86c6f8463774447d23f17a
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-27 00:13:01 +0300

    Add files via upload

---
+++ SoftUni - Recursion and Backtracking/Recursion and Backtracking - Lab/04. Recursive Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _4._Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Multiply(n));
+        }
+        private  static int Multiply(int n)
+        {
+            if (n == 0)
+            { 
+                return 1;
+            }
+            else
+            {
+                return n * Multiply(n - 1);
+            }
+        }
+    }
+}
