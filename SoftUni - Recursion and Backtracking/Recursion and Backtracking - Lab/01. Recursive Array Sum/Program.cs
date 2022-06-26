8d5e806b580241fdb7215fdbb4dac3aec8b51be5
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-27 00:04:47 +0300

    Add files via upload

---
+++ SoftUni - Recursion and Backtracking/Recursion and Backtracking - Lab/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Recursive_Array_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(Sum(numbers, 0));
+        }
+        private static int Sum(int[] numbers, int num)
+        {
+            if (num == numbers.Length - 1)
+            { 
+                return numbers[num];
+            }
+
+            return numbers[num] + Sum(numbers,num + 1);
+        }
+    }
+}
