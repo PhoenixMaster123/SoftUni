28425b1038a3b160f0c166392c3640d2d49ec6b0
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-27 00:09:44 +0300

    Add files via upload

---
+++ SoftUni - Recursion and Backtracking/Recursion and Backtracking - Lab/03. Generating 0-1 Vectors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Generating_0_1_Vectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int [n];
+
+            Gener01(arr, 0);
+        }
+        private static void Gener01(int[] arr, int com)
+        {
+            if (com >= arr.Length)
+            {
+                Console.WriteLine(string.Join(string.Empty, arr));
+                return;
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                arr[com] = i;
+
+                Gener01(arr, com + 1);
+            }
+        }
+    }
+}
