9b3b87b5778ff0662c1be04d63c0f54ba285194e
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-27 00:07:00 +0300

    Add files via upload

---
+++ SoftUni - Recursion and Backtracking/Recursion and Backtracking - Lab/02. Recursive Drawing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Recursive_Drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Symbols(n);
+        }
+        private static void Symbols(int figures)
+        {
+            if (figures == 0) 
+            {
+                return;
+            }
+            Console.WriteLine(new string('*', figures));
+
+            Symbols(figures - 1);
+
+            Console.WriteLine(new string('#', figures));
+
+        }
+    }
+}
