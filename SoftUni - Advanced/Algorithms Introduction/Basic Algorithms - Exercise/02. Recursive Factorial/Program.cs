ee25e9a9030f4153c85bd7fc84c50e9459bae346
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-28 21:23:43 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Algorithms Introduction/Basic Algorithms - Exercise/02. Recursive Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02._Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(num));
+        }
+
+        private static long Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * Factorial(num - 1);
+            }
+        }
+    }
+}
