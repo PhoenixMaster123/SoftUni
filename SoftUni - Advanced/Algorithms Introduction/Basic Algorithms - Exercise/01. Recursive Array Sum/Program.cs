b53d6306c9ff3d7d8245b6cbb25c876dc27e50e7
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-28 20:07:29 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Algorithms Introduction/Basic Algorithms - Exercise/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] arrNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+           long sum = SumArray(arrNumbers, 0);
+           Console.WriteLine(sum);
+        }
+
+        private static long SumArray(int[] arrNumbers, int startIndex)
+        {
+            if (startIndex == arrNumbers.Length - 1) // bottom of recursion
+            {
+                return arrNumbers[startIndex];
+            }
+            return arrNumbers[startIndex] + SumArray(arrNumbers, startIndex + 1);
+        }
+    }
+}
