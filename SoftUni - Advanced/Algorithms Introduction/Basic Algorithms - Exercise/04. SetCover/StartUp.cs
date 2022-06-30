33b35aef40544f22c7bcb15810fd7b448c614f1d
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-06-30 12:51:16 +0300

    Add files via upload

---
+++ SoftUni - Advanced/Algorithms Introduction/Basic Algorithms - Exercise/04. SetCover/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace SetCover
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+          int[] universe = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+          int numberOfSets = int.Parse(Console.ReadLine());
+          int[][] jaggedSets = new int[numberOfSets][];
+
+            for (int row = 0; row < jaggedSets.Length; row++)
+            {
+                int[] rowsValue = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                jaggedSets[row] = new int[rowsValue.Length];
+                for (int col = 0; col < jaggedSets[row].Length; col++)
+                {
+                    jaggedSets[row][col] = rowsValue[col];
+                }
+            }
+            List<int[]> selectedSets = ChooseSets(jaggedSets.ToList(), universe.ToList());
+            Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+            foreach(int[] set in selectedSets)
+            {
+                Console.WriteLine($"{{ {string.Join(", ",set )} }} ");
+               
+            }
+        }
+
+        public static List<int[]> ChooseSets(List<int[]> sets, IList<int> universe)
+        {
+            List<int[]> selectedSets = new List<int[]>();
+
+            while(universe.Count > 0)
+            {
+                int[] current = sets.OrderByDescending(set => set.Count(universe.Contains)).First();
+                selectedSets.Add(current);
+                sets.Remove(current);
+                foreach (int i in current) 
+                {
+                    universe.Remove(i);
+                }
+            }
+            return selectedSets;
+        }
+    }
+}
