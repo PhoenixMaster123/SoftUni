1725a8294bbed7fb5a7fa96b1c0ae8785b4fc7f3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-01 23:37:55 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/Drawing Figures with Loops/Drawing Figures with Loops - Exercise/01. Rectangle of 10 x 10 Stars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01._Rectangle_of_10_x_10_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= num; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
