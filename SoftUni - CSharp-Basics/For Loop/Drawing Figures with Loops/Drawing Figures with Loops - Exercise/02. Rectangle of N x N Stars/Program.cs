9ec057c91384fface6d86d60ab46787bc8c761b3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-01 23:38:50 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/Drawing Figures with Loops/Drawing Figures with Loops - Exercise/02. Rectangle of N x N Stars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Rectangle_of_N_x_N_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rectangle = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= rectangle; row++)
+            {
+                for (int col = 1; col <= rectangle; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
