89518966b237906300caaaf7439c84462953ce8d
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-01 23:39:38 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/Drawing Figures with Loops/Drawing Figures with Loops - Exercise/03. Square of Stars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Square_of_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int square = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= square; row++)
+            {
+                for (int col = 0; col < square; col++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
