32d60a324ecb43924c70b9e15a2e0e8f80238573
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-01 23:40:27 +0300

    Add files via upload

---
+++ SoftUni - CSharp-Basics/For Loop/Drawing Figures with Loops/Drawing Figures with Loops - Exercise/04. Triangle of Dollars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04._Triangle_of_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int triangle = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= triangle; row++)
+            {
+                for (int j = 1; j <= row; j++)
+                {
+                    Console.Write("");
+                }
+                for (int k = 1; k <= row; k++)
+                {
+                    Console.Write("$ ");
+                }
+                Console.WriteLine("");
+            }
+           
+        }
+    }
+}
