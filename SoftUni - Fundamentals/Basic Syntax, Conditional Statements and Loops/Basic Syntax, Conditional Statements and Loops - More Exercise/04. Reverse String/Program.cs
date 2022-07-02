b14e3c2b90cf556f8220c48638d21be3e93a43a7
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-02 23:43:24 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - More Exercise/04. Reverse String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+            for (int reverseIndex = username.Length - 1; reverseIndex >= 0; reverseIndex--)
+            {
+                password += username[reverseIndex];
+            }
+            Console.Write(password);
+        }
+    }
+}
