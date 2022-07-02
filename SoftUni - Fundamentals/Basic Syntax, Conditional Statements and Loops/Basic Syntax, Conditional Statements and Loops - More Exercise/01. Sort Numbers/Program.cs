9f8d3b3a3d4dbb50f7b3535b79a720131fad3654
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-02 23:01:05 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Basic Syntax, Conditional Statements and Loops/Basic Syntax, Conditional Statements and Loops - More Exercise/01. Sort Numbers/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _01._Sort_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int x;
+
+            if (b < a)
+            {
+                x = a;
+                a = b;
+                b = x;
+            }
+            if (c < a)
+            {
+                x = c;
+                c = a;
+                a = x;
+            }
+            if (c < b)
+            {
+                x = c;
+                c = b;
+                b = x;
+            }
+            if (a == b)
+            {
+                x = a;
+                a = b;
+                b = x;
+            }
+            if (a == c)
+            {
+                x = a;
+                a = c;
+                c = x;
+            }
+            if (b == c)
+            {
+                x = b;
+                b = c;
+                c = x;
+            }
+            Console.WriteLine(c);
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+        }
+    }
+    }
+
+
