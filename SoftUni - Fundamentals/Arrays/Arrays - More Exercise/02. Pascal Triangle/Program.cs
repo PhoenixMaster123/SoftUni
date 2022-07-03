fa9909697ab58123b9daae03188ab8f379a3634f
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-03 21:47:56 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Arrays/Arrays - More Exercise/02. Pascal Triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long triangleRows = long.Parse(Console.ReadLine());
+
+            long[][] jaggedArray = new long[triangleRows][];
+
+            jaggedArray[0] = new long[1];
+            jaggedArray[0][0] = 1;
+
+
+            for (long row = 1; row < jaggedArray.Length; row++)
+            {
+                jaggedArray[row] = new long[row + 1];
+                jaggedArray[row][0] = 1;
+                jaggedArray[row][jaggedArray[row].Length - 1] = 1;
+
+                for (long col = 1; col < jaggedArray[row].Length - 1; col++)
+                {
+                    long leftDiagonal = jaggedArray[row - 1][col - 1];
+                    long rightDiagonal = jaggedArray[row - 1][col];
+
+                    jaggedArray[row][col] = leftDiagonal + rightDiagonal;
+                }
+            }
+
+            for (long row = 0; row < triangleRows; row++)
+            {
+                Console.WriteLine(string.Join(" ", jaggedArray[row]));
+            }
+        }
+    }
+}
