a9fc24fed4f6a379266bfafca10e0c1195f58ae2
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-05 21:53:06 +0300

    Add files via upload

---
+++ SoftUni - C# OOP/Inheritance/Inheritance - Exercise/Person/Child.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        // I want to change the logic of the settet -> to have two validations 
+
+        /////////////////////////////////////////////////////////////////// First Variant ////////////////////////////////
+        //public override int Age 
+        //{ get => base.Age; 
+        //  set => base.Age = value; }
+        /////////////////////////////////////////////////////////////////// Second Variant ////////////////////////////////
+        //public override int Age 
+        //{
+        //    get 
+        //    { 
+        //        return base.Age; 
+        //    }
+        //    set
+        //    {
+        //        if(value > 15)
+        //        {
+        //            base.Age = 15;
+        //        }
+        //        else
+        //        {
+        //            base.Age = value;
+        //        }
+              
+        //    }
+        //}
+        public Child(string name, int age) : base(name, age)
+        {
+          
+        }
+    }
+}
---
+++ SoftUni - C# OOP/Inheritance/Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person  // by default inheritate Object
+    {
+
+        // Fields are used only in getters and setters of the properties
+
+        private string name; // we use them when we need make validation 
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name; 
+            this.Age = age;
+        }
+        // Properties are used in Constructors, Methods, and public instances
+        
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public int Age // for override -> public virtual int Age
+        { 
+            get 
+            {
+                return this.age;
+            } 
+            set 
+            {
+                if (this.age < 0) // Validation
+                {
+                    this.age = 0;
+                }
+                else
+                {
+                    this.age = value;
+                }
+                
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+
+
+    }
+}
