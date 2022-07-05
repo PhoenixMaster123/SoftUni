5886946ab30dda7cad41cd552715c241c16f4f1f
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-05 21:59:53 +0300

    Add files via upload

---
+++ SoftUni - C# OOP/Inheritance/Inheritance - Exercise/02. Zoo/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+        public override string ToString()
+        {
+            return $"{this.Name}";
+        }
+    }
+}
