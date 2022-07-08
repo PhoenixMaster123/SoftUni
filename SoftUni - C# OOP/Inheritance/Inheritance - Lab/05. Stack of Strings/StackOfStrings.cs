445146bac3ee6aa3823596fd398bcb9755e407a3
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-08 14:04:38 +0300

    Add files via upload

---
+++ SoftUni - C# OOP/Inheritance/Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+        public void AddRange(Stack<string> collection)
+        {
+            foreach(string item in collection)
+            {
+                this.Push(item);
+            }
+        }
+    }
+}
+   
+       
+  
