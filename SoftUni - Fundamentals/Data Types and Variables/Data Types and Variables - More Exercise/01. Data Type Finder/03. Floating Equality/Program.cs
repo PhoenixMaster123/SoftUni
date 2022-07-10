77e09ddefb1b1cda371e90287b0e2ecc5328fb4f
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-10 23:39:39 +0300

    Add files via upload

---
+++ SoftUni - Fundamentals/Data Types and Variables/Data Types and Variables - More Exercise/01. Data Type Finder/03. Floating Equality/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _03._Floating_Equality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////////////////////////////////// First Variant //////////////////////////////////////
+            double eps = 0.000001;
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            double result = Math.Abs(num1 - num2);
+            if(result < eps)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            /////////////////////////////////////////////////////////////////////// Second Variant //////////////////////////////////////
+            //bool isEqual = Math.Abs(num1 - num2) < eps;
+
+            //if (isEqual)
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+        }
+    }
+}
