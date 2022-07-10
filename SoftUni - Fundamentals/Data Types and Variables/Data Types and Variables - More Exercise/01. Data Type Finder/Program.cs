a1ab176482b76d1b21aaeaadd99dd5122f6fc8fb
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-10 23:42:00 +0300

    Update Program.cs

--- SoftUni - Fundamentals/Data Types and Variables/Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
+++ SoftUni - Fundamentals/Data Types and Variables/Data Types and Variables - More Exercise/01. Data Type Finder/Program.cs
@@ -6,7 +6,33 @@ namespace _01._Data_Type_Finder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string command = Console.ReadLine();
+            
+            while (command != "END")
+            {
+                if (int.TryParse(command, out int valueInt))
+                {
+                    Console.WriteLine($"{command} is integer type");
+                }
+                else if (float.TryParse(command, out float valueFloat))
+                {
+                    Console.WriteLine($"{command} is floating point type");
+                }
+                else if (char.TryParse(command, out char valueChar))
+                {
+                    Console.WriteLine($"{command} is character type");
+                }
+                else if (bool.TryParse(command, out bool valueBoolean))
+                {
+                    Console.WriteLine($"{command} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{command} is string type");
+                }
+
+                command = Console.ReadLine();
+            }
         }
     }
 }
