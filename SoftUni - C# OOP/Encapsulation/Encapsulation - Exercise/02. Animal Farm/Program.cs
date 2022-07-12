f8f9b2e33d2cf0f90e823f510fd6e7562154c8d4
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-12 19:36:19 +0300

    Update Program.cs

--- SoftUni - C# OOP/Encapsulation/Encapsulation - Exercise/02. Animal Farm/Program.cs
+++ SoftUni - C# OOP/Encapsulation/Encapsulation - Exercise/02. Animal Farm/Program.cs
@@ -12,11 +12,7 @@
                 int age = int.Parse(Console.ReadLine());
 
                 Chicken chicken = new Chicken(name, age);
-                Console.WriteLine(
-                    "Chicken {0} (age {1}) can produce {2} eggs per day.",
-                    chicken.Name,
-                    chicken.Age,
-                    chicken.ProductPerDay);
+                Console.WriteLine("Chicken {0} (age {1}) can produce {2} eggs per day.", chicken.Name, chicken.Age, chicken.ProductPerDay);
             }
             catch (ArgumentException ex)
             {
