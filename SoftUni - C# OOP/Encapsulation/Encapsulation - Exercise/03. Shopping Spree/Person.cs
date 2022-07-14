d1393fd0dc352ecb174b16f67374ffc364c45d64
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-14 18:27:40 +0300

    Add files via upload

---
+++ SoftUni - C# OOP/Encapsulation/Encapsulation - Exercise/03. Shopping Spree/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping_Spree
+{
+    class Person
+    {
+        private string name;
+        private decimal money;
+        public List<Product> BagOfProducts { get; set; }
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.BagOfProducts = new List<Product>();
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+        public string CanAfford(Product product)
+        {
+            if (this.Money >= product.Cost)
+            {
+                this.AddProduct(product);
+                return $"{this.Name} bought {product.Name}";
+            }
+            else
+            {
+                return $"{this.Name} can't afford {product.Name}";
+            }
+        }
+        private void AddProduct(Product product)
+        {
+            this.BagOfProducts.Add(product);
+            this.Money -= product.Cost;
+        }
+        public override string ToString()
+        {
+            if (this.BagOfProducts.Count == 0)
+            {
+                return $"{this.Name} - Nothing bought";
+            }
+            else
+            {
+                return $"{this.Name} - {string.Join(", ", this.BagOfProducts.Select(p => p.Name))}";
+
+            }
+        }
+    }
+}
---
+++ SoftUni - C# OOP/Encapsulation/Encapsulation - Exercise/03. Shopping Spree/StartUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping_Spree
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string[] namesSpree = Console.ReadLine().Split(new char[] { ';', '=' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            List<Person> humans = new List<Person>();
+            for (int k = 0; k < namesSpree.Length; k += 2)
+            {
+                try
+                {
+                    Person person = new Person(namesSpree[k], int.Parse(namesSpree[k + 1]));
+                    humans.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
+            }
+            namesSpree = Console.ReadLine().Split(new char[] { ';', '=' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            List<Product> products = new List<Product>();
+            for (int k = 0; k < namesSpree.Length; k += 2)
+            {
+                try
+                {
+                    Product product = new Product(namesSpree[k], int.Parse(namesSpree[k + 1]));
+                    products.Add(product);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Environment.Exit(0);
+                }
+            }
+            string[] cmd = Console.ReadLine().Split(" ").ToArray();
+            while (cmd[0]?.ToUpper() != "END")
+            {
+                string personName = cmd[0];
+                string productName = cmd[1];
+
+                Person currentPerson = humans.FirstOrDefault(p => p.Name == personName);
+                Product currentProduct = products.FirstOrDefault(p => p.Name == productName);
+                if (currentPerson != null && currentProduct != null)
+                {
+                    Console.WriteLine(currentPerson.CanAfford(currentProduct));
+                }
+
+                cmd = Console.ReadLine().Split(" ").ToArray();
+            }
+
+            humans.ForEach(p => Console.WriteLine(p));
+        }
+    }
+}
