0ceff18689c08ed52aff64d4518bf1b34d8cda99
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-15 23:32:47 +0300

    Add files via upload

---
+++ SoftUni - C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/01.DefineAnInterfaceIPerson/IPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    // Convetion -> Most of the interfaces shoud be public 
+    // Define public side of our class: propetries (getter,setter) , methods
+    public interface IPerson
+    {
+        public string Name { get; }
+        public int Age { get; }
+    }
+       
+       
+}
---
+++ SoftUni - C# OOP/Interfaces and Abstraction/Interfaces and Abstraction - Exercise/01.DefineAnInterfaceIPerson/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // Interface -> public side of our class
+            // Interface -> define public visibility of our class 
+            // Interface -> communication between classes
+            // Using Interface -> abstracton level increasment
+            // Abstraction in Business logic of the application -> Core class 
+            // Work with interfaces without knowing the concrete implementation
+            string name = Console.ReadLine(); 
+            int age = int.Parse(Console.ReadLine()); 
+            IPerson person = new Citizen(name, age); 
+            Console.WriteLine(person.Name); 
+            Console.WriteLine(person.Age);
+        }
+    }
+}
