f3a0eb193ba7e6a971b1cf08f25e3163f53e3e52
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-17 23:40:36 +0300

    Competition

---
+++ SoftUni - Softuniada/Softuniada 2020/01. Dream Car/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Softuniada_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double startsalary = double.Parse(Console.ReadLine());
+            double costs = double.Parse(Console.ReadLine());
+            double newsolary = double.Parse(Console.ReadLine());
+            double carcosts = double.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            double sum = startsalary;
+
+
+            for (int i = 1; i < month; i++)
+            {
+                
+                startsalary += newsolary;
+                sum += startsalary;
+
+            }
+            costs *= month;
+            double total = sum - costs;
+            if(total>=carcosts)
+            {
+                Console.WriteLine("Have a nice ride!");
+            }
+            else
+            {
+                Console.WriteLine("Work harder!");
+            }
+        }
+    }
+}
