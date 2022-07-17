3116ff590ff35ef3f7f7dd907c89386aaefe1e1e
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-18 01:15:30 +0300

    Competition

---
+++ SoftUni - Softuniada/Softuniada 2020/02. New SoftUni Building/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _02._New_SoftUni_Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int height = size + size / 2;
+            for (int i = 0; i < height; i++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    int result = i + k;
+                    if (result % 4 == 0)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
