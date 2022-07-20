846c29fab1991260f03d125248337f8f02da97c2
Кристиан Попов <99142736+PhoenixMaster123@users.noreply.github.com>
2022-07-20 06:36:47 +0300

    Finding area and perimeter of rectangle and circle

---
+++ SoftUni - C# OOP/Polymorphism/Polymorphism - Lab/03.Shapes/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+
+        public Rectangle(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public double Width { get;  set; }
+        public double Height { get;  set; }
+        public override double CalculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (this.Width * 2 + this.Height * 2);
+        }
+        public override string Draw()
+        {
+            return base.Draw() + GetType().Name;
+        }
+
+    }
+}
+
