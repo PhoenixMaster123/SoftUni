43e9f01dfd0c1c1fd56cb5cb96c1ca7d7e655ef2
Кристиан Попов <kristianpopov03@yahoo.com>
2022-07-22 17:28:52 +0300

    Exception

---
+++ SoftUni - C# OOP/Exception Handling/Exception Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int num = int.Parse(Console.ReadLine());
+            try
+            {
+
+
+                if (num < 0)
+                {
+                    //Console.WriteLine("Invalid number."); 
+                    throw new Exception("Invalid number.");
+                    
+                }
+                else
+                {
+                    double result = Math.Sqrt(num);
+                    Console.WriteLine(result);
+                    Console.WriteLine("Goodbye.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Goodbye.");
+            }
+           
+           
+            
+        }
+    }
+}
