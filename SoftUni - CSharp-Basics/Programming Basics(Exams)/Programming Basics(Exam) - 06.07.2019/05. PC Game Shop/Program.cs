e4658b913ec7553e1cb05425c33c0743d441686f
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-04 01:31:10 +0300

    05. PC Game Shop - For Loop

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics(Exam) - 06.07.2019/05. PC Game Shop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _05._PC_Game_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countGames = int.Parse(Console.ReadLine());
+            double hearthstone = 0;
+            double fornite = 0;
+            double overwatch = 0;
+            double others = 0;
+            double percent = 100;
+            for (int i = 0; i < countGames; i++)
+            {
+                string games = Console.ReadLine();
+                if(games == "Hearthstone")
+                {
+                    hearthstone++;
+                }
+                else if(games == "Fornite")
+                {
+                    fornite++;
+                }
+                else if (games == "Overwatch")
+                {
+                    overwatch++;
+                }
+                else
+                {
+                    others++;
+                }
+            }
+            hearthstone = (percent / countGames) * hearthstone;
+            fornite = (percent / countGames) * fornite;
+            overwatch = (percent / countGames) * overwatch;
+            others = (percent / countGames) * others;
+            Console.WriteLine($"Hearthstone - {hearthstone:F2}%");
+            Console.WriteLine($"Fornite - {fornite:F2}%");
+            Console.WriteLine($"Overwatch - {overwatch:F2}%");
+            Console.WriteLine($"Others - {others:F2}%");
+        }
+    }
+}
