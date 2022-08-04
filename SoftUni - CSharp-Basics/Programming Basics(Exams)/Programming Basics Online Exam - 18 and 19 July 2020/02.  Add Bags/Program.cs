895ca5c307726e7c7e1ac2e38b7a5ebfd45fbc91
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-05 00:48:07 +0300

    02. Add Bags - Basic If Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 18 and 19 July 2020/02.  Add Bags/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace T2___Add_Bags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double luggage = double.Parse(Console.ReadLine());
+            double kilograms = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int countluggage = int.Parse(Console.ReadLine());
+            double priceluggage = 0;
+            //double priceluggage = luggage / countluggage;
+            if(kilograms<10)
+            {
+                priceluggage = luggage * 0.2;
+            }
+            else if(kilograms>=10&&kilograms<=20)
+            {
+                priceluggage = luggage * 0.5;
+                
+            }
+            else if(kilograms>20)
+            {
+                priceluggage = luggage;
+            }
+            if (days > 30)
+            {
+                priceluggage = priceluggage + priceluggage * 0.1;
+            }
+            else if (days >= 7 && days <= 30)
+            {
+                priceluggage = priceluggage + priceluggage * 0.15;
+            }
+            else
+            {
+                priceluggage = priceluggage + priceluggage * 0.4;
+            }
+            double result = priceluggage * countluggage;
+            Console.WriteLine($"The total price of bags is: {result:F2} lv.");
+        }
+    }
+}
