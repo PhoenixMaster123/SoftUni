55520e02137fc4f232a63acbc14ffa76ffffbfef
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-05 00:47:00 +0300

    01. Agency Profit - Basic Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 18 and 19 July 2020/01.  Agency Profit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T1___Agency_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string company = Console.ReadLine();
+            int eldern = int.Parse(Console.ReadLine());
+            int kids = int.Parse(Console.ReadLine());
+            double neteldernprice = double.Parse(Console.ReadLine());
+            double served = double.Parse(Console.ReadLine());
+
+            double kidsprice = neteldernprice - neteldernprice * 0.7;
+            double elderprice = neteldernprice + served;
+
+            double allkidsprice = kidsprice + served;
+
+            double result = (kids * allkidsprice) + (eldern * elderprice);
+
+            double profit = result * 0.2;
+            Console.WriteLine($"The profit of your agency from {company} tickets is {profit:F2} lv.");
+
+       
+        }
+    }
+}
