85e179308373456a246fad8c0d1d0b5873c2b3ef
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-05 00:58:40 +0300

    05. Best Player - While Loop

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 18 and 19 July 2020/05. Best Player/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace T5___Best_Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* string player = Console.ReadLine();
+             int score = 0;
+             int playerscore = 0;
+             while (player!="END")
+             {
+
+                 int goals = int.Parse(Console.ReadLine());
+                 score += goals;
+                 if(score>0)
+                 {
+                     playerscore += score;
+                     if(playerscore>goals)
+                     {
+
+                     }
+                 }
+
+                 player = Console.ReadLine();
+
+             }*/
+                                        
+                string player = Console.ReadLine();
+                string newPlayer = "";
+                int bestScore = 0;                
+                while (player != "END")
+                {
+                    int currentGoals = int.Parse(Console.ReadLine());
+                    if (currentGoals > bestScore)
+                    {
+                        newPlayer = player;
+                        bestScore = currentGoals;
+                    }
+
+                    if (bestScore >= 10)
+                    {
+                        break;
+                    }
+                  player = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{newPlayer} is the best player!");
+                if (bestScore >= 3)
+                {
+                    Console.WriteLine($"He has scored {bestScore} goals and made a hat-trick !!!");
+                }
+                else
+                {
+                    Console.WriteLine($"He has scored {bestScore} goals.");
+                }
+            
+        }
+    }
+
+}
+    
+
