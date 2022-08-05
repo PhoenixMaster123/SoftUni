4fa87e2bcf67ff805bc8eb9ecb17d5cee56cc071
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-05 19:05:30 +0300

    01. Fruit Market - Basic Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics(Exam) -  02.05.2019/01. Fruit Market/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Fruit_Market
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double strawberryPrice = double.Parse(Console.ReadLine());
+            double kgbananas = double.Parse(Console.ReadLine());
+            double kgorange = double.Parse(Console.ReadLine());
+            double kgraspberries = double.Parse(Console.ReadLine());
+            double kgstrawberries = double.Parse(Console.ReadLine());
+
+            double rasberryPrice = strawberryPrice / 2;
+            double orangePrice = rasberryPrice - rasberryPrice * 0.4;
+            double bananasPrice = rasberryPrice - rasberryPrice * 0.8;
+
+            double resultRasberry = kgraspberries * rasberryPrice;
+            double resultOrange = kgorange * orangePrice;
+            double resultBananas = kgbananas * bananasPrice;
+            double resultStrawberries = strawberryPrice * kgstrawberries;
+
+            double total = resultRasberry + resultOrange + resultBananas + resultStrawberries;
+            Console.WriteLine($"{total:F2}");
+        }
+    }
+}
