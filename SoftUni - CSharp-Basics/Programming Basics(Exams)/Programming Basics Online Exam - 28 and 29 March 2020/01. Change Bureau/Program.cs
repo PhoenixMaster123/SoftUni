c9a0578fab1c27f3012616e534e76c9cfa39624a
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-06 01:20:55 +0300

    01. Change Bureau - Basic Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 28 and 29 March 2020/01. Change Bureau/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace T1___Change_Bureau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bitcoin = int.Parse(Console.ReadLine());
+            double joan = double.Parse(Console.ReadLine());
+            double comision = double.Parse(Console.ReadLine());
+            comision = comision / 100;
+            
+
+            bitcoin = bitcoin * 1168;
+            joan = joan * 0.15;
+            double dolar = 1.76;
+            double euro = 1.95;
+
+            double lev = joan * dolar;
+            double result = lev + bitcoin;
+            result = result / euro;
+            comision = result * comision;
+            double result2 = result - comision;
+            Console.WriteLine($"{result2:F2}");
+
+
+        }
+    }
+}
