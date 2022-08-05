6c47a8428e6b8a68b12a443e162be013dde1e2ba
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-06 01:36:00 +0300

    01. Birthday Party - Basic Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 28 and 29 March 2020/01. Birthday Party/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T1___Birthday_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rent = double.Parse(Console.ReadLine());
+            double tortePrice = rent * 0.2;
+            double drinks = tortePrice - tortePrice * 0.45;
+            double animator = rent / 3;
+            double total = rent + tortePrice + drinks + animator;
+            Console.WriteLine($"{total}");
+        }
+    }
+}
