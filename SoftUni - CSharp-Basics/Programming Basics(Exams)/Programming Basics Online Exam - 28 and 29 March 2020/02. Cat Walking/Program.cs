315c8d61ec38e0cb20bfada0da3d184fdb0bdf2e
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-06 01:22:04 +0300

    02. Cat Walking - Basic If Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 28 and 29 March 2020/02. Cat Walking/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace T2___Cat_Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesperday = int.Parse(Console.ReadLine());
+            int walkperday = int.Parse(Console.ReadLine());
+            double calories = int.Parse(Console.ReadLine());
+
+            double alltime = walkperday * minutesperday;
+            alltime *= 5;
+
+            calories = calories * 0.5;
+            if(alltime>=calories)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {alltime}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {alltime}.");
+            }
+             
+
+
+        }
+    }
+}
