63f46972c5e553a0035b15515eb8ba47681000b2
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-07 09:45:08 +0300

    03. Guild - OOP/Methods

---
+++ SoftUni - Advanced/C# Advanced Exam/C# Advanced Exam - 22 Feb 2020/03. Guild/Guild.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    public class Guild
+    {
+        List<Player> roster;
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public Guild(string name,int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.roster = new List<Player>();
+        }
+        public void AddPlayer(Player player)
+        {
+            if(this.Capacity > 0 && !roster.Any(x => x.Name == player.Name))
+            {
+                this.roster.Add(player);
+            }    
+        }
+        public bool RemovePlayer(string name)
+        {
+            if (this.roster.Any(x => x.Name == name)) 
+            {               
+                this.roster.RemoveAll(x => x.Name == name);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void PromotePlayer(string name)
+        {           
+            if (roster.Any(x => x.Name == name))
+            {
+                Player promotedPlayer = roster.Where(x => x.Name == name).FirstOrDefault();
+                promotedPlayer.Rank = "Member";
+            }
+
+        }
+        public void DemotePlayer(string name)
+        {
+            if (roster.Any(x => x.Name == name))
+            {
+                Player demotedPlayer = roster.Where(x => x.Name == name).FirstOrDefault();
+                demotedPlayer.Rank = "Trial";
+            }            
+        }
+        public Player[] KickPlayersByClass(string @class)
+        {
+
+            List<Player> kickPlayerList = new List<Player>();
+            foreach (var player in this.roster)
+            {
+                if (player.Class == @class)
+                {
+                    kickPlayerList.Add(player);
+                }
+            }
+            Player[] myArrayToReturn = kickPlayerList.ToArray();
+
+            this.roster = this.roster.Where(x => x.Class != @class).ToList();
+
+            return myArrayToReturn;
+
+        }
+        public int Count => this.roster.Count;
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Players in the guild: {this.Name}");
+            foreach(var players in roster)
+            {
+                sb.AppendLine(players.ToString());
+                //sb.AppendLine($"Player {players.Name}: {players.Class}");
+                //sb.AppendLine($"Rank: {players.Rank}");
+                //sb.AppendLine($"Description: {players.Description}");
+            }
+            return sb.ToString().TrimEnd();
+
+        }
+
+
+    }
+}
---
+++ SoftUni - Advanced/C# Advanced Exam/C# Advanced Exam - 22 Feb 2020/03. Guild/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; } = "Trial";
+        public string Description { get; set; } = "n/a";
+
+        public Player(string name, string @class)
+        {
+            this.Name = name;
+            this.Class = @class;
+        }
+        public override string ToString()
+        {
+            return $"Player {this.Name}: {this.Class}" + Environment.NewLine +
+                   $"Rank: {this.Rank}" + Environment.NewLine + 
+                   $"Description: {this.Description}";
+            
+;
+        }
+
+    }
+}
