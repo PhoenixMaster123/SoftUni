b4152c6c0b337f126bd6a9235d4481927a811d08
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-08 11:24:52 +0300

    02. Mountain Run - Basic If Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 28 and 29 March 2020/02. Mountain Run/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02._Mountain_Run
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeInSeconds = double.Parse(Console.ReadLine());
+
+            double allSeconds = distanceInMeters * timeInSeconds;
+            double addSeconds = (Math.Floor(distanceInMeters / 50)) * 30;
+            double finalTime = allSeconds + addSeconds;
+            if (finalTime >= recordInSeconds)
+            {
+                finalTime -= recordInSeconds;
+                Console.WriteLine($"No! He was {finalTime:F2} seconds slower.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes! The new record is {finalTime:F2} seconds.");
+            }
+
+        }
+    }
+}
