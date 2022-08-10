90e942457d6251a9439bdb2f4162025707d22811
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-10 12:50:28 +0300

    04. Trekking Mania - For Loop

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 28 and 29 March 2020/04. Trekking Mania/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _04._Trekking_Mania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peoplesGroup = int.Parse(Console.ReadLine());
+            double allPeople = 0;
+            double musulaPeople = 0;
+            double monbanPeople = 0;
+            double kilimanjaroPeople = 0;
+            double k2People = 0;
+            double everestPeople = 0;
+            for (int i = 0; i < peoplesGroup; i++)
+            {
+                int countPeopleOnGroup = int.Parse(Console.ReadLine());
+                allPeople += countPeopleOnGroup;
+                if (countPeopleOnGroup <= 5)
+                {
+                    musulaPeople += countPeopleOnGroup;
+                }
+                else if (countPeopleOnGroup >= 6 && countPeopleOnGroup <= 12)
+                {
+                    monbanPeople += countPeopleOnGroup;
+                }
+                else if (countPeopleOnGroup >= 13 && countPeopleOnGroup <= 25)
+                {
+                    kilimanjaroPeople += countPeopleOnGroup;
+                }
+                else if (countPeopleOnGroup >= 26 && countPeopleOnGroup <= 40)
+                {
+                    k2People += countPeopleOnGroup;
+                }
+                else if (countPeopleOnGroup >= 41)
+                {
+                    everestPeople += countPeopleOnGroup;
+                }
+            }
+            Console.WriteLine($"{musulaPeople / allPeople * 100:F2}%");
+            Console.WriteLine($"{monbanPeople / allPeople * 100:F2}%");
+            Console.WriteLine($"{kilimanjaroPeople / allPeople * 100:F2}%");
+            Console.WriteLine($"{k2People / allPeople * 100:F2}%");
+            Console.WriteLine($"{(everestPeople / allPeople) * 100:F2}%");
+        }
+    }
+}
