9b36719e0e945e01b884d525e84b5c0bce999009
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-13 00:22:16 +0300

    01. Series Calculator - Basic Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 15 and 16 June 2019/01. Series Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Series_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string serial = Console.ReadLine();
+            int seasons = int.Parse(Console.ReadLine());
+            int episodes = int.Parse(Console.ReadLine());
+            double time = double.Parse(Console.ReadLine());
+
+            double ads = time * 0.20;
+            double adsAndSerails = time + ads;
+            double specialEpisodes = seasons * 10;
+
+            double totalTime = adsAndSerails * episodes * seasons + specialEpisodes;
+            Console.WriteLine($"Total time needed to watch the {serial} series is {totalTime} minutes.");
+        }
+    }
+}
