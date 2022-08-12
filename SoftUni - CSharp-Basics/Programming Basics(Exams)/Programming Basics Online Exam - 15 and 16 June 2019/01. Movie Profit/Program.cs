7ce806c09088e1e29a412df6defbb5f6c5eaa888
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-13 00:25:48 +0300

    01. Movie Profit - Basic Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 15 and 16 June 2019/01. Movie Profit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Movie_Profit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            int countTickets = int.Parse(Console.ReadLine());
+            double ticketsPrice = double.Parse(Console.ReadLine());
+            double percentCinema = int.Parse(Console.ReadLine());
+
+            double pricetickets = countTickets * ticketsPrice;
+            double allperiod = days * pricetickets;
+            double percent = percentCinema / 100;
+
+            double result = allperiod - allperiod * percent;
+            Console.WriteLine($"The profit from the movie {name} is {result:F2} lv.");
+        }
+    }
+}
