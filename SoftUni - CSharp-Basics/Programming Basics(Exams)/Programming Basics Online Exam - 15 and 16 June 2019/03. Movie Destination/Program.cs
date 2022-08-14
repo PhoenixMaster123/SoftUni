f7629e5fa6a09419e7bf75c52737c2beb63a09bb
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-15 00:11:43 +0300

    03. Movie Destination - Advance If Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 15 and 16 June 2019/03. Movie Destination/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _03._Movie_Destination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double movieBudget = double.Parse(Console.ReadLine());
+            string destination = Console.ReadLine();
+            string season = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            double total = 0;
+            if(destination == "Dubai")
+            {
+                if(season == "Winter")
+                {
+                    double price = 45000;
+                    total = price * days;
+                }
+                else
+                {
+                    double price = 40000;
+                    total = price * days;
+                }
+                total -= total * 0.30;
+            }
+            else if (destination == "Sofia")
+            {
+                if (season == "Winter")
+                {
+                    double price = 17000;
+                    total = price * days;
+                }
+                else
+                {
+                    double price = 12500;
+                    total = price * days;
+                }
+                total += total * 0.25;
+            }
+            else
+            {
+                if (season == "Winter")
+                {
+                    double price = 24000;
+                    total = price * days;
+                }
+                else
+                {
+                    double price = 20250;
+                    total = price * days;
+                }
+                
+            }
+            if(total > movieBudget) 
+            {
+                total -= movieBudget;
+                Console.WriteLine($"The director needs {total:F2} leva more!");
+                
+            }
+            else
+            {
+                movieBudget -= total;
+                Console.WriteLine($"The budget for the movie is enough! We have {movieBudget:F2} leva left!");
+            }
+        }
+    }
+}
