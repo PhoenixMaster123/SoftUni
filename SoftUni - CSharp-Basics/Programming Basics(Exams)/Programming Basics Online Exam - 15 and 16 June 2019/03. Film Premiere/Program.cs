860fb52c6a64fedf4528634b4734fd022f998bb9
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-14 23:59:52 +0300

    03. Film Premiere - Advance If Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 15 and 16 June 2019/03. Film Premiere/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace _03._Film_Premiere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+            string paketForMovie = Console.ReadLine();
+            int tickets = int.Parse(Console.ReadLine());
+            double drink = 0;
+            double popcorn = 0;
+            double menu = 0;
+            double total = 0;
+            if (movie.Equals("John Wick"))
+            {
+                if(paketForMovie == "Drink")
+                {
+                    drink = 12;
+                    total += drink * tickets;
+                }
+                else if(paketForMovie == "Popcorn")
+                {
+                    popcorn = 15;
+                    total += popcorn * tickets;
+                }
+                else
+                {
+                    menu = 19;
+                    total += menu * tickets;
+                }
+            }
+            else if (movie.Equals("Star Wars"))
+            {
+                if (paketForMovie == "Drink")
+                {
+                    drink = 18;
+                    total += drink * tickets;
+                }
+                else if (paketForMovie == "Popcorn")
+                {
+                    popcorn = 25;
+                    total += popcorn * tickets;
+                }
+                else
+                {
+                    menu = 30;
+                    total += menu * tickets;
+                }
+                if(tickets >= 4)
+                {
+                    total -= total * 0.30;
+                }
+            }
+            else
+            {
+                if (paketForMovie == "Drink")
+                {
+                    drink = 9;
+                    total += drink * tickets;
+                }
+                else if (paketForMovie == "Popcorn")
+                {
+                    popcorn = 11;
+                    total += popcorn * tickets;
+                }
+                else
+                {
+                    menu = 14;
+                    total += menu * tickets;
+                }
+                if(tickets == 2)
+                {
+                    total -= total * 0.15;
+                }
+            }
+            Console.WriteLine($"Your bill is {total:F2} leva.");
+        }
+    }
+}
