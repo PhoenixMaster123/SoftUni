0b6425b58f8f3e7b1a53a9300537bdba7de79b35
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-14 17:20:24 +0300

    02. Movie Day - Basic If Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 15 and 16 June 2019/02. Movie Day/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02._Movie_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeForPhotos = int.Parse(Console.ReadLine());
+            int scenes = int.Parse(Console.ReadLine());
+            int range = int.Parse(Console.ReadLine());
+
+            double preparationField = Math.Floor(timeForPhotos * 0.15);
+            double timeForScenes = scenes * range;
+            double allTime = preparationField + timeForScenes;
+
+            if (timeForPhotos < allTime)
+            {
+                allTime -= timeForPhotos;
+                Console.WriteLine($"Time is up! To complete the movie you need {Math.Ceiling(allTime)} minutes.");
+            }
+            else
+            {
+                allTime -= timeForPhotos;
+                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Abs(allTime)} minutes left!");
+            }
+        }
+    }
+}
