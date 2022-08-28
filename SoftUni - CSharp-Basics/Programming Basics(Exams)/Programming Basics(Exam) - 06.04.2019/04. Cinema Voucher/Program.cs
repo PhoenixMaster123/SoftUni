b7b10dfd551012a596439b36569745261cf70f27
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-28 20:39:08 +0300

    04. Cinema Voucher - While Loop + ASCII Table

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics(Exam) - 06.04.2019/04. Cinema Voucher/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _04._Cinema_Voucher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int voucherValue = int.Parse(Console.ReadLine());
+            string command;
+            int priceMovie = 0;
+            int priceOthers = 0;
+            char[] chars;
+            int tickets = 0;
+            int others = 0;
+            while((command = Console.ReadLine())!="End")
+            {
+                string purchase = command;
+                
+                if(purchase.Length > 8)
+                {
+                    chars = purchase.ToCharArray();
+                    for (int i = 0; i < chars.Length; i++)
+                    {
+                        if(i < 2)
+                        {
+                            priceMovie+=chars[i];        
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+                    if(priceMovie <= voucherValue)
+                    {
+                        voucherValue -= priceMovie;
+                        tickets++;
+                        priceMovie = 0;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                   
+                }
+                else
+                {
+                    chars = purchase.ToCharArray();
+                    for (int i = 0; i < chars.Length; i++)
+                    {
+                        if (i < 1)
+                        {
+                            priceOthers += chars[i];
+                            
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+                    if (priceOthers <= voucherValue)
+                    {
+                        voucherValue -= priceOthers;
+                        others++;
+                        priceOthers = 0;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    
+                }        
+            }
+            Console.WriteLine(tickets);
+            Console.WriteLine(others);
+        }
+    }
+}
