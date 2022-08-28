2b4489af040556f57af61dad918d3a12036efe6d
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-29 00:37:20 +0300

    06. Name Game - Nested Loops + ASCII Table

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics(Exam) - 06.07.2019/06. Name Game/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Name_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int asciiTable = 0;
+            int finalScore = 0;
+            string WinnerName = string.Empty; 
+            string command;
+            while ((command = Console.ReadLine()) != "Stop")
+            {
+                int points = 0;
+                string name = command;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    asciiTable = (int)name[i];
+
+                    int num = int.Parse(Console.ReadLine()); 
+
+                    if (asciiTable == num)
+                    {
+                        points += 10;
+                    }
+                    else
+                    {
+                        points += 2;
+                    }
+
+                    if (points >= finalScore) 
+                    {
+                        finalScore = points;
+                        WinnerName = name;
+                    }
+                } 
+               
+            }
+
+            Console.WriteLine($"The winner is {WinnerName} with {finalScore} points!");
+        }
+    }
+}
