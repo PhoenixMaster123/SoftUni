ef03952fe802ae850c21fbd2e6621653a369d899
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-30 00:07:51 +0300

    01. Easter Lunch - Basic Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 20 and 21 April 2019/01. Easter Lunch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Easter_Lunch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cozunaci = int.Parse(Console.ReadLine());
+            int eggs = int.Parse(Console.ReadLine());
+            int kgCookies = int.Parse(Console.ReadLine());
+
+            double cozunaciPrice = cozunaci * 3.20;
+            double eggPrice = eggs * 4.35;
+            double cookiesPrice = kgCookies * 5.40;
+            double paintEggs = eggs * 12 * 0.15;
+            double total = cozunaciPrice + eggPrice + cookiesPrice + paintEggs;
+            Console.WriteLine($"{total:F2}");
+        }
+    }
+}
