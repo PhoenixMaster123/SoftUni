25649689a3c6ffaf8d07f800c2b9752d2dbe77aa
Kristian Popov <kristianpopov03@yahoo.com>
2022-08-31 15:06:03 +0300

    01. Easter Bakery - Basic Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 20 and 21 April 2019/01. Easter Bakery/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Easter_Bakery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double flourPrice = double.Parse(Console.ReadLine());
+            double flourAmount = double.Parse(Console.ReadLine());
+            double sugarAmount = double.Parse(Console.ReadLine());
+            int eggsPeel = int.Parse(Console.ReadLine());
+            int yeastAmount = int.Parse(Console.ReadLine());
+
+            double sugarPrice = flourPrice * 0.75;
+            double oneEggsPeel = flourPrice * 1.1;
+            double yeastPrice = sugarPrice * 0.2;
+
+            double flourTotal = flourPrice * flourAmount;
+            double sugarTotal = sugarPrice * sugarAmount;
+            double eggsTotal = eggsPeel * oneEggsPeel;
+            double yeastTotal = yeastPrice * yeastAmount;
+
+            Console.WriteLine($"{flourTotal + sugarTotal + eggsTotal + yeastTotal:F2}");
+        }
+    }
+}
