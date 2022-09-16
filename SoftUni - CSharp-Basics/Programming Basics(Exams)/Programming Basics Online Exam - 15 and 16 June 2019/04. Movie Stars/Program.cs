59bb45976f870a4567175cf45e8d847c2409c745
Kristian Popov <kristianpopov03@yahoo.com>
2022-09-16 17:05:21 +0200

    04. Movie Stars - While Loop

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 15 and 16 June 2019/04. Movie Stars/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04._Movie_Stars1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string command;
+            while ((command = Console.ReadLine()) != "ACTION")
+            {
+                string actor = command;
+               
+                if (actor.Length > 15)
+                {
+                    budget -= budget *= 0.20;
+                    continue;
+                }
+                double salary = double.Parse(Console.ReadLine());
+                budget -= salary;
+                if (budget <= 0)
+                {
+                   
+                    break;
+                }
+
+            }
+            if (budget <= 0)
+            {
+                Console.WriteLine($"We need {Math.Abs(budget):F2} leva for our actors.");
+               
+            }
+            else
+            {
+                Console.WriteLine($"We are left with {budget:F2} leva.");
+            }
+           
+        }
+                    
+    }
+}
