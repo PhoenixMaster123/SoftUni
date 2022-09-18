cb7577c3dc428a5fa149d5399ac08ba5a261cd71
Kristian Popov <kristianpopov03@yahoo.com>
2022-09-18 22:44:14 +0200

    05. Oscars - For Loop

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 15 and 16 June 2019/05. Oscars/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _05._Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string actor = Console.ReadLine();
+            double academicPoints = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            double sumPoint = academicPoints;
+            bool isFound = false;
+            for (int i = 0; i < n; i++)
+            {
+
+                string jury = Console.ReadLine();
+                double juryPoints = double.Parse(Console.ReadLine());
+
+                double momentsPoints = (jury.Length * juryPoints) / 2;
+                sumPoint += momentsPoints;
+                if (sumPoint >= 1250.5)
+                {
+                    isFound = true;
+                    break;
+                }
+
+            }
+            if (isFound)
+            {
+                Console.WriteLine($"Congratulations, {actor} got a nominee for leading role with {sumPoint:F1}!");
+            }
+            else
+            {
+                double neededpoints = 1250.5 - sumPoint;
+                Console.WriteLine($"Sorry, {actor} you need {neededpoints:F1} more!");
+            }
+        }
+    }
+}
