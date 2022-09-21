5508c5835a1431c2461284733571ebb3264109dc
Kristian Popov <kristianpopov03@yahoo.com>
2022-09-21 16:55:10 +0200

    06. Favorite Movie - Nested Loops

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 15 and 16 June 2019/06. Favorite Movie/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Favorite_Movie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            int points = 0;
+            int movieCount = 0;
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            while((command = Console.ReadLine())!="STOP")
+            {
+                string movies = command;
+                movieCount++;
+                if (movieCount == 7)
+                {
+                    Console.WriteLine("The limit is reached.");
+                    break;   
+                }
+                foreach (char currentChar in movies)
+                {
+                    if (char.IsUpper(currentChar))
+                    {
+                        points += currentChar - movies.Length;
+                    }
+                    else if (char.IsLower(currentChar))
+                    {
+                        points += currentChar -  2 * movies.Length ;
+                    }
+                    else
+                    {
+                        points += currentChar;
+                    }     
+                }
+                map.Add(movies, points);
+                points = 0;
+            }            
+             foreach(var dictionary in map.OrderByDescending(x=>x.Value))
+             {
+                    Console.WriteLine($"The best movie for you is {dictionary.Key} with {dictionary.Value} ASCII sum.");
+                    break;
+             }
+            
+        }
+    }
+}
