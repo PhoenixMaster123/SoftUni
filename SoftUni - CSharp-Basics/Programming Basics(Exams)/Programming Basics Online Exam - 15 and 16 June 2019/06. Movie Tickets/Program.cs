9f800ddf7ff98e47d5e6cd0a0298e5693462db9e
Kristian Popov <kristianpopov03@yahoo.com>
2022-09-25 13:03:55 +0200

    06. Movie Tickets - Nested Loops

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 15 and 16 June 2019/06. Movie Tickets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06._Movie_Tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            char symbol1;
+            int symbol2;
+            int symbol3;
+            int symbol4;
+            for (int i = a1; i <= a2-1; i++)
+            {
+                
+                symbol1 = (char)i;
+                for (int k = 1; k <= n - 1; k++)
+                {
+                    symbol2 = k;
+                    for (int x = 1; x <= n / 2 - 1; x++)
+                    {
+                        symbol3 = x;
+                        symbol4 = (int)symbol1;
+                        if (symbol4 % 2 != 0)
+                        {
+                            if ((x + k + i) % 2 == 1 && i % 2 == 1)
+                            {
+                                Console.WriteLine($"{symbol1}-{symbol2}{symbol3}{symbol4}");
+                            }
+                           
+                        }
+                    }
+                }
+               
+            }    
+        }
+    }
+}
