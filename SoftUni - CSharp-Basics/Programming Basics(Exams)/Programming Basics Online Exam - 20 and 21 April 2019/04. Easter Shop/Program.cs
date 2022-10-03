174b77b3af0134df8356a33fbf09d4d89a8e4fec
Kristian Popov <kristianpopov03@yahoo.com>
2022-10-03 13:06:54 +0200

    04. Easter Shop - While Loop

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 20 and 21 April 2019/04. Easter Shop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _04._Easter_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allEggs = int.Parse(Console.ReadLine());
+            string command;
+            int allSellEggs = 0;
+            while ((command = Console.ReadLine()) != "Close") 
+            {
+                string action = command;
+                int eggs = int.Parse(Console.ReadLine());
+                
+                if (action == "Fill")
+                {
+                    allEggs += eggs;
+                }
+                else if(action == "Buy")
+                {
+                    allEggs -= eggs;
+                    allSellEggs += eggs;
+                    if (allEggs < 0)
+                    {
+                        allEggs += eggs;
+                        Console.WriteLine("Not enough eggs in store!");
+                        Console.WriteLine($"You can buy only {allEggs}.");
+                        break;
+                    }
+                }
+               
+                
+            }
+            if (command == "Close")
+            {
+                Console.WriteLine("Store is closed!");
+                Console.WriteLine($"{allSellEggs} eggs sold.");
+               
+            }
+        }
+    }
+}
