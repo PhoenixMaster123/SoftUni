20781b9dc3fb6873da84532dd021103f6b1ddebc
Kristian Popov <kristianpopov03@yahoo.com>
2022-10-03 12:31:07 +0200

    03. Easter Trip - Advance If Operations

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 20 and 21 April 2019/03. Easter Trip/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _03._Easter_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            string dates = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+            int total = 0;
+            if(country == "France")
+            {
+                if(dates == "21-23")
+                {
+                    total = 30 * nights;
+                }
+                else if(dates == "24-27")
+                {
+                    total = 35 * nights;
+                }
+                else
+                {
+                    total = 40 * nights;
+                }
+            }
+            else if(country == "Italy")
+            {
+                if (dates == "21-23")
+                {
+                    total = 28 * nights;
+                }
+                else if (dates == "24-27")
+                {
+                    total = 32 * nights;
+                }
+                else
+                {
+                    total = 39 * nights;
+                }
+            }
+            else
+            {
+                if (dates == "21-23")
+                {
+                    total = 32 * nights;
+                }
+                else if (dates == "24-27")
+                {
+                    total = 37 * nights;
+                }
+                else
+                {
+                    total = 43 * nights;
+                }
+            }
+            Console.WriteLine($"Easter trip to {country} : {total:F2} leva.");
+        }
+    }
+}
