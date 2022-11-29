8b8121f5aa35685ab222ec1c13088475b51da329
Kristian Popov <kristianpopov03@yahoo.com>
2022-11-30 00:36:01 +0100

    06. Easter Competition - Nested Loops

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 20 and 21 April 2019/06. Easter Competition/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Easter_Competition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countCosunats = int.Parse(Console.ReadLine());
+            string note = string.Empty;
+            string currentChef = string.Empty;
+
+            int maxPoints = int.MinValue;
+
+            for (int i = 1; i <= countCosunats; i++)
+            {
+                string nameChef = Console.ReadLine();
+                note = Console.ReadLine();
+                int points = 0;
+
+                while (note != "Stop")
+                {
+                    int grade = int.Parse(note);
+
+                    if (grade <= 0 || grade > 10)
+                    {
+                        note = Console.ReadLine();
+                        continue;
+                    }
+
+                    points += grade;
+                    note = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{nameChef} has {points} points.");
+
+                if (points > maxPoints)
+                {
+                    maxPoints = points;
+                    currentChef = nameChef;
+                    Console.WriteLine($"{currentChef} is the new number 1!");
+                }
+            }
+
+            Console.WriteLine($"{currentChef} won competition with {maxPoints} points!");
+        }
+    }
+}
+
