bac12a5bdebdca370497a109719261fd91d3f819
Kristian Popov <kristianpopov03@yahoo.com>
2022-12-12 16:20:14 +0100

    06. Easter Decoration - Nested Loops

---
+++ SoftUni - CSharp-Basics/Programming Basics(Exams)/Programming Basics Online Exam - 20 and 21 April 2019/06. Easter Decoration/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _06._Easter_Decoration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int customers = int.Parse(Console.ReadLine());
+            double sum = 0;
+            double total = 0;
+            int count = 0;
+            for (int i = 0; i < customers; i++)
+            {
+                while(true)
+                {
+                    string products = Console.ReadLine();
+                    if(products == "Finish")
+                    {
+                        if(count %2 == 0)
+                        {
+                            sum = sum - sum * 0.20;
+                        }
+                        total += sum;
+                        Console.WriteLine($"You purchased {count} items for {sum:F2} leva.");
+                        sum = 0;
+                        count = 0;
+                        break;
+                    }    
+                    if(products == "basket")
+                    {
+                        sum += 1.50;
+                        count++;
+                    }
+                    else if(products == "wreath")
+                    {
+                        sum += 3.80;
+                        count++;
+                    }
+                    else if(products == "chocolate bunny")
+                    {
+                        sum += 7;
+                        count++;
+                    }
+                }    
+            }
+            Console.WriteLine($"Average bill per client is: {total/customers:F2} leva.");
+        }
+    }
+}
