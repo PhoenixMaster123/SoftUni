afb2106d2ba9c9096d11df61078ac5782fc20b0e
Kristian Popov <kristianpopov03@yahoo.com>
2023-01-11 01:26:45 +0200

    Add files via upload

---
+++ SoftUni - C++ Basic/First Steps In Coding/First Steps In Coding - Lab/04. Inches to Centimeters/04. Inches to Centimeters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double inches;
+    cin >> inches;
+    double centimeturs;
+    centimeturs = inches * 2.54;
+    cout << centimeturs;
+    return 0;
+}
