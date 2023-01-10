c8953f2b96f81935f19d1bc0a476915d265c0a43
Kristian Popov <kristianpopov03@yahoo.com>
2023-01-11 01:24:23 +0200

    03. Rectangle Area

---
+++ SoftUni - C++ Basic/First Steps In Coding/First Steps In Coding - Lab/03. Rectangle Area/03. Rectangle Area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count;
+    cin >> count;
+    int num;
+    cin >> num;
+    int result = count * num;
+    cout << result;
+    return 0;
+}
