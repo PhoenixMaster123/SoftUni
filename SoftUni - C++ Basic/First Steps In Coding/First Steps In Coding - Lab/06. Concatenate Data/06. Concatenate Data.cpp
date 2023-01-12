12d4c08c2cdb640f721e99395c680c5da679098d
Kristian Popov <kristianpopov03@yahoo.com>
2023-01-12 23:01:20 +0200

    06. Concatenate Data

---
+++ SoftUni - C++ Basic/First Steps In Coding/First Steps In Coding - Lab/06. Concatenate Data/06. Concatenate Data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	string firstName, lastName;
+	cin >> firstName >> lastName;
+	int age;
+	cin >> age;
+	string town;
+	cin >> town;
+	cout << "You are " << firstName << " " << lastName << ", a " << age << "-years old person from " << town << "." << endl;
+}
