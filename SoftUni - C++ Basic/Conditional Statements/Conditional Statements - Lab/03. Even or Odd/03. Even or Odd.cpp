a6c5cee7751afe37afb1f16cb5e4a1070e4d78d9
Kristian Popov <kristianpopov03@yahoo.com>
2023-02-05 23:45:00 +0100

    03. Even or Odd

---
+++ SoftUni - C++ Basic/Conditional Statements/Conditional Statements - Lab/03. Even or Odd/03. Even or Odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1;
+	cin >> num1;
+	if (num1 % 2 == 0)
+	{
+		cout << "even" << endl;
+	}
+	else
+	{
+		cout << "odd" << endl;
+	}
+	return 0;
+}
+
