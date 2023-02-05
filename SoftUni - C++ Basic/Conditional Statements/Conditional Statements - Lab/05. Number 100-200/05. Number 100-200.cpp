82ca181df4cb0bec71704727de8c91daadda27b0
Kristian Popov <kristianpopov03@yahoo.com>
2023-02-05 23:54:40 +0100

    05. Number 100...200

---
+++ SoftUni - C++ Basic/Conditional Statements/Conditional Statements - Lab/05. Number 100-200/05. Number 100-200.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number;
+	cin >> number;
+	if (number < 100)
+	{
+		cout << "Less than 100" << endl;
+	}
+	else if(number >= 100 && number <= 200)
+	{
+		cout << "Between 100 and 200" << endl;
+	}
+	else
+	{
+		cout << "Greater than 200" << endl;
+	}
+	return 0;
+}
+
