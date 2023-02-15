8e48cd534be3d4e5231166a98767341637dbc1ca
Kristian Popov <kristianpopov03@yahoo.com>
2023-02-15 20:56:44 +0100

    10. Invalid Number

---
+++ SoftUni - C++ Basic/Conditional Statements Advanced/Conditional Statements Advanced - Lab/10. Invalid Number/10. Invalid Number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin >> num;
+	if (num >= 100 && num <= 200 || num == 0)
+	{
+
+	}
+	else
+	{
+
+		cout << "invalid" << endl;
+	}
+}
