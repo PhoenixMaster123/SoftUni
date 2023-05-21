a4a06cb1470283111775acfe0a1e72cf2c50c9b3
Kristian Popov <kristianpopov03@yahoo.com>
2023-05-21 22:51:47 +0300

    05. Greeting by Name

---
+++ SoftUni - Java Basics/First Steps In Coding/First Steps In Coding - Lab/GreetingbyName.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class GreetingbyName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.print("Hello, " + name + "!");
+    }
+}
+
