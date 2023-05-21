dc4067a5d20159fa3d6b2eb74a636e3286dc633a
Kristian Popov <kristianpopov03@yahoo.com>
2023-05-21 22:51:05 +0300

    04. Inches to Centimeters

---
+++ SoftUni - Java Basics/First Steps In Coding/First Steps In Coding - Lab/InchestoCentimeters.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class InchestoCentimeters {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            double inches = Double.parseDouble(scanner.nextLine());
+            double centimeters = inches * 2.54;
+            System.out.println(centimeters);
+        }
+    }
+
