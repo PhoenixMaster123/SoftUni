4de5d247540950f4feeb5b0efae6075c77b520f9
Kristian Popov <kristianpopov03@yahoo.com>
2023-09-29 12:58:20 +0200

    06. Equal Arrays

--- SoftUni - Java/Java-Fundamentals/Arrays/Arrays - Lab/06. Equal Arrays/src/equalArrays/Main.java
+++ SoftUni - Java/Java-Fundamentals/Arrays/Arrays - Lab/06. Equal Arrays/src/equalArrays/Main.java
@@ -1,14 +1,71 @@
 package equalArrays;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+///////////////////////// Third Variant /////////////////////////
 		Scanner sc = new Scanner(System.in);
+        int [] items = Arrays.stream(sc.nextLine().split(" ")).mapToInt(e -> Integer.parseInt(e)).toArray();
+        int[] numbers = Arrays.stream(sc.nextLine().split(" ")).mapToInt(e -> Integer.parseInt(e)).toArray();
+        int sum = 0;         
+        //bool flag = false;
+        for ( int i = 0; i < items.length; i++)
+        {
+            sum += items[i];
+            if (items[i] != numbers[i])
+            {
+                System.out.printf("Arrays are not identical. Found difference at %d index.",i);
+               // flag = true;
+                return;
+               // break;
+                
+            }   
+        }
+        System.out.printf("Arrays are identical. Sum: %d", sum);
+        //if (!flag)
+        //{
+        //    System.out.printf("Arrays are identical. Sum: %d", sum);
+        //}  
+
+
+///////////////////////// Second Variant /////////////////////////
+		/*Scanner scanner = new Scanner(System.in);
+
+        String[] itemsString = scanner.nextLine().split(" ");
+        String[] numbersString = scanner.nextLine().split(" ");
+
+        int[] items = new int[itemsString.length];
+        int[] numbers = new int[numbersString.length];
+
+        for (int i = 0; i < itemsString.length; i++) {
+            items[i] = Integer.parseInt(itemsString[i]);
+        }
+
+        for (int i = 0; i < numbersString.length; i++) {
+            numbers[i] = Integer.parseInt(numbersString[i]);
+        }
+
+        int sum = 0;
+        boolean flag = false;
+
+        for (int i = 0; i < items.length; i++) {
+            sum += items[i];
+            if (items[i] != numbers[i]) {
+                System.out.printf("Arrays are not identical. Found difference at %d index.", i);
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag) {
+            System.out.printf("Arrays are identical. Sum: %d%n", sum);
+        }
+        */
+///////////////////////// First Variant /////////////////////////
+		/* Scanner sc = new Scanner(System.in);
 		
 		int[] numbers = Arrays.stream(sc.nextLine().split(" "))
 				.mapToInt(e -> Integer.parseInt(e)).toArray();
@@ -38,6 +95,7 @@ public class Main {
 			}
 			break;
 		}
+		*/
 	}
 
 }
