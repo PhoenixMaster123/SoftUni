e9113bcfa593066d32108e3b9261bff6ba35eb60
Kristian Popov <kristianpopov03@yahoo.com>
2023-10-05 17:11:50 +0200

    03. House Party

--- SoftUni - Java/Java-Fundamentals/List/List - Exercise/03. House Party/src/Main.java
+++ SoftUni - Java/Java-Fundamentals/List/List - Exercise/03. House Party/src/Main.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
