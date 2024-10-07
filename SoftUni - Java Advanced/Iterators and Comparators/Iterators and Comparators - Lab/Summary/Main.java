76de21023504214d0f4fd4dd63c85b1311f28b5c
Kristian Popov <kristianpopov03@yahoo.com>
2024-10-07 23:05:12 +0300

    Iterators and Comparators

---
+++ SoftUni - Java Advanced/Iterators and Comparators/Iterators and Comparators - Lab/Summary/Main.java
@@ -0,0 +1,16 @@
+package Summary;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Iterable → make the class to can iterate (implements Iterable<type>)
+
+        // 2. Iterator → private class, which knows how this class to iterate
+        // (implements Iterator<type> (hasNext, next)
+
+        // 3. Comparable → make this class to can compare the objects (implements Comparable<type>)
+        // (method: compareTo(Object o)
+
+        // 4. Comparator → outside class that knows how this class is to compare two objects
+        // (method: compare(Object o1, Object o2)
+    }
+}
