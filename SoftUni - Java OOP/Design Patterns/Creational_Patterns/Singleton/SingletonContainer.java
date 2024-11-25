f457afb3f05415e37ce6a42299449b77f2186532
Kristian Popov <kristianpopov03@yahoo.com>
2024-11-25 21:00:53 +0100

    Creational Patterns

---
+++ SoftUni - Java OOP/Design Patterns/Creational_Patterns/Singleton/SingletonContainer.java
@@ -0,0 +1,10 @@
+package Creational_Patterns.Singleton;
+
+import java.util.Map;
+
+public interface SingletonContainer {
+
+    int getPopulation(Map<String, Integer> capitals, String name);
+}
+
+
